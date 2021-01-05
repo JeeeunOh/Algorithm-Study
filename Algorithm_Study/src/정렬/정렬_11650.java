afb3d589907f842dce2dc37ef8da7e927a23118e
0909oje <0909oje@naver.com>
2021-01-05 16:10:59 +0900

    0105_21

---
+++ Algorithm_Study/src/정렬/정렬_11650.java
@@ -0,0 +1,10 @@
+package 정렬;
+
+public class 정렬_11650 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Algorithm_Study/src/정렬/정렬_2751.java
@@ -0,0 +1,20 @@
+package 정렬;
+
+import java.util.*;
+
+public class 정렬_2751 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for(int i=0;i<n;i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+}

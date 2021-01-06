c53696a59246100edf1761d90b6afc52f766bbf8
0909oje <0909oje@naver.com>
2021-01-06 19:21:42 +0900

    compareTo 메소드

---
+++ Algorithm_Study/src/정렬/ex10814.java
@@ -0,0 +1,36 @@
+package 정렬;
+
+import java.util.*;
+ 
+public class ex10814 {
+	public static void main(String[] args) {		
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		String[][] arr = new String[N][2];
+		
+ 
+		for(int i = 0; i < N; i++) {
+			arr[i][0] = in.next();	// 나이
+			arr[i][1] = in.next();	// 이름
+		}
+ 
+		
+		Arrays.sort(arr, new Comparator<String[]>() {
+			// 나이순으로 정렬
+			@Override
+			public int compare(String[] s1, String[] s2) {
+				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+			}
+			
+		});
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+        
+		
+	}
+ 
+}

3c81de0c6ee4d1d13b98645cdbe08516835271f8
0909oje <0909oje@naver.com>
2021-01-14 21:53:25 +0900

    Array.sort()는 시간초과, Quick Selection

--- Algorithm_Study/src/정렬/ex11004.java
+++ Algorithm_Study/src/정렬/ex11004.java
@@ -1,10 +1,9 @@
 package 정렬;
+// N개의 수에서 K번 째 수를 반환하는 경우에 사용되는특정한 알고리즘인  Quick Selection
+import java.io.*;
 
 public class ex11004 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
 	}
-
 }

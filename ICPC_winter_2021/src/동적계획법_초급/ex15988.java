41bd0684831af04fbffa1f7521842e85b5c9891a
0909oje <0909oje@naver.com>
2021-01-21 17:18:00 +0900

    arr[j]=arr[j-1]+arr[j-2]+arr[j-3];

--- ICPC_winter_2021/src/동적계획법_초급/ex15988.java
+++ ICPC_winter_2021/src/동적계획법_초급/ex15988.java
@@ -8,17 +8,17 @@ public class ex15988 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		arr = new long [1000001];
+		arr = new long [1000000];
 		arr[1]=1;
 		arr[2]=2;
 		arr[3]=4;
 		for (int i=0 ; i < n ; i++){
 			num = sc.nextInt();
-			if (num<=3) System.out.println(arr[num]);
+			if (num<=3) System.out.println(arr[num]%1000000009);
 			else {
 				for (int j=4 ; j<=num ; j++)
 					arr[j]=arr[j-1]+arr[j-2]+arr[j-3];
-				System.out.println(arr[num]);
+				System.out.println(arr[num]%1000000009);
 			}
 		}
 	}

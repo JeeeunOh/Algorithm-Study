162b99feb16bd890595bdb4ee03727253e210474
0909oje <0909oje@naver.com>
2021-01-21 22:54:29 +0900

    9번틀림 시간초과:BufferedReader, 런타임에러:long arr[] 범위 +1 해줌

--- ICPC_winter_2021/src/동적계획법_초급/ex15988.java
+++ ICPC_winter_2021/src/동적계획법_초급/ex15988.java
@@ -1,23 +1,28 @@
 package 동적계획법_초급;
 
-import java.util.*;
+import java.io.*;
 
 public class ex15988 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long mod = 1000000009L;
+		int lim = 1000000;
 		int num;
-		long[] arr = new long [1000000];
+		long[] arr = new long [lim+1];
+		arr[0]=1;
 		arr[1]=1;
 		arr[2]=2;
 		arr[3]=4;
-		for (int i=0 ; i < n ; i++){
-			num = sc.nextInt();
-			if (num>3) {
-				for (int j=4 ; j<=num ; j++)
-					arr[j]=(arr[j-1]%1000000009)+(arr[j-2]%1000000009)+(arr[j-3]%1000000009);
-			}
-			System.out.println(arr[num]%1000000009);
+		for (int i=4 ; i <=lim; i++){
+					arr[i]=((arr[i-1]%mod)+(arr[i-2]%mod)+(arr[i-3]%mod))%mod;
+		}
+		int n = Integer.parseInt(br.readLine());
+		for (int i=0 ; i<n ; i++) {
+			num = Integer.parseInt(br.readLine());
+			bw.write(String.valueOf(arr[num]));
+			bw.write('\n');
+			bw.flush(); // flush() : 현재 버퍼에 저장되어 있는 내용을 클라이언트로 전송하고 버퍼를 비운다.
 		}
 	}
 }

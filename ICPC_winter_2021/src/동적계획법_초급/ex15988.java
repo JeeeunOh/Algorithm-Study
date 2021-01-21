c1c1ec973620fd5298f1b0fde2d6ff658a4f5cdf
0909oje <0909oje@naver.com>
2021-01-21 17:42:51 +0900

    모든 배열에 %1000000009 하는 것 중요

--- ICPC_winter_2021/src/동적계획법_초급/ex15988.java
+++ ICPC_winter_2021/src/동적계획법_초급/ex15988.java
@@ -3,24 +3,21 @@ package 동적계획법_초급;
 import java.util.*;
 
 public class ex15988 {
-	static int num;
-	static long arr[];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		arr = new long [1000000];
+		int num;
+		long[] arr = new long [1000000];
 		arr[1]=1;
 		arr[2]=2;
 		arr[3]=4;
 		for (int i=0 ; i < n ; i++){
 			num = sc.nextInt();
-			if (num<=3) System.out.println(arr[num]%1000000009);
-			else {
+			if (num>3) {
 				for (int j=4 ; j<=num ; j++)
-					arr[j]=arr[j-1]+arr[j-2]+arr[j-3];
-				System.out.println(arr[num]%1000000009);
+					arr[j]=(arr[j-1]%1000000009)+(arr[j-2]%1000000009)+(arr[j-3]%1000000009);
 			}
+			System.out.println(arr[num]%1000000009);
 		}
 	}
-	
 }

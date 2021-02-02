f44c417d6eee04d04eafcf8eb8e58e792a19dfb4
0909oje <0909oje@naver.com>
2021-02-02 23:24:45 +0900

    스택 오등큰수 주석달기

--- EDOC/src/스택_큐_덱/ex17299.java
+++ EDOC/src/스택_큐_덱/ex17299.java
@@ -5,45 +5,31 @@ import java.io.*;
 
 public class ex17299{
     public static void main(String args[]) throws IOException {
-        // 리소스 선언 및 초기화
-        Stack<Integer> s = new Stack<Integer>(); // 등장 횟수 비교 시 자료구조(스택) 이용
+        Stack<Integer> s = new Stack<Integer>(); // 등장 횟수 비교 -> 스택
         
-        // input 입력
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 수열의 크기
-        int[] a = new int[n]; // 수열 저장할 배열
-        int[] f = new int[1000001]; // 등장한 횟수 저장할 배열 (1<=원소<=1,000,000)
-        int[] ngf = new int[n]; // 오등큰수를 저장할 배열
-        String[] temp = br.readLine().split(" ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 수열 크기
+        int[] a = new int[n]; // 수열 저장 배열
+        int[] f = new int[1000001]; // 등장 횟수 배열
+        int[] ngf = new int[n]; // 오등큰수 저장 배열
+        
         for(int i = 0; i < n; i++) { // 수열 만들기
-            a[i] = Integer.parseInt(temp[i]);
-            f[a[i]] += 1;
+            a[i] = sc.nextInt();
+            f[a[i]] += 1; // 해당 숫자 count++ 해주기
         }
         
-        s.push(0);
+        s.push(0); // 처음 인덱스는 그냥 넣고
         for (int i = 0; i < n; i++) {
-            if(s.isEmpty()) {
-                s.push(i);
+            if(s.isEmpty())  s.push(i); // 스택 비어있으면 바로 지금 인덱스 저장
+            while(!s.isEmpty() && f[a[s.peek()]] < f[a[i]]) { 
+            	 // 스택이 비어있지 않고 숫자가 스택 가장 윗 숫자와 비교해서 큰 경우 반복
+                ngf[s.pop()] = a[i]; // 스택 하나 비우면서 이 인덱스에 숫자 넣음
             }
-            while(!s.isEmpty() && f[a[s.peek()]] < f[a[i]]) {
-                // 오등큰수 계산
-                ngf[s.pop()] = a[i];
-            }
+            // 반복 끝나면, 다음에 비교할 값 stack에 저장
             s.push(i);
         }
-
-        while(!s.isEmpty()) {
-            // 최종 스택에 남아있는 인덱스들을 기준으로 해당 위치의 a[i]의 오등큰수는 존재하지 않는다.
-            ngf[s.pop()] = -1;
-        }
-        
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int num : ngf) {
-            bw.append(num + " ");
-        }
-        
-        bw.write("\n");
-        bw.flush();
-        
+        // 최종 스택에 남아있는 숫자들의 오등큰수는 존재 x -> -1 값 넣어줌
+        while(!s.isEmpty())  ngf[s.pop()] = -1; 
+        for (int i=0 ; i< n ; i ++) System.out.print(ngf[i]+" ");
     }
 }

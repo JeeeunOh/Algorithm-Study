043302b3e5a58442b406e00a2557ea09834602ba
0909oje <0909oje@naver.com>
2021-02-10 13:48:14 +0900

    인, 포스트 오더 통해 프리오더 출력함

--- EDOC/src/그래프트리_210210/ex2263.java
+++ EDOC/src/그래프트리_210210/ex2263.java
@@ -1,18 +1,11 @@
 package 그래프트리_210210;
-
- // 1+ 2+ 4+ 8+...
-// 어려운점 : 트리도 어려운데 분할정복이라 더 어려움,,젠장
-
-// 인오더와 포스트오더 주어짐 -> 프리오더 찾자
-// 인오더 : 좌 -> 루트 -> 우
-// 프리오더 : 루트 -> 좌 -> 우
-// 포스트오더 : 좌 -> 우 -> 루트
-/* 이런식으로 출력 : 풀이를 보니 별로 상관없어보임
- *public void preorder(Node root){
-		System.out.print(root.data); //먼저 가운데 출력
-		if(root.left!=null) preorder(root.left); //그 다음 왼쪽
-		if(root.right!=null) preorder(root.right); //마지막 오른쪽
-	}
+/*
+ 어려운점 : 트리도 어려운데 분할정복이라 더 어려움,,젠장
+ 문제 ) n개의 정점을 갖는 이진 트리의 정점에 1부터 n까지의 번호가 중복 없이 매겨져 있다
+			인오더와 포스트오더 주어짐 -> 프리오더 찾자
+푸는 방법)
+1. 포스트오더의 끝을 루트로 정하고 프리오더로 답을 출력해야하므로 일단 출력
+2. 루트를 기준으로 인오더의 왼쪽 오른쪽을 나눠서 재귀탐색
  */
 
 import java.util.Scanner;
@@ -22,27 +15,31 @@ public class ex2263{
     static int [] postOrder = new int[100000];
     static int [] position = new int[100001];
     
+   /* - 인오더 : 좌 -> 루트 -> 우
+    - 포스트오더 : 좌 -> 우 -> 루트 : 맨 마지막에 입력 받는 것이 루트이다.
+    -   - 프리오더 : 루트 -> 좌 -> 우 : 출력해야하는 것
+    */
+    
+    // 인오더 시작과 끝, 포스트오더 시작과 끝을 입력받음
     static void solve(int is, int ie, int ps, int pe) {
-        if(is>ie || ps>pe) return ;
-        int root = postOrder[pe];
+        if(is>ie || ps>pe) return ; //이 if문에 해당하면 재귀가 끝난 것이므로 재귀 종료
+        int root = postOrder[pe]; // 포스트오더 마지막이 루트임 -> 루트 출력
         System.out.print(root+" ");
-        int inRoot = position[root]; //인오더의 루트 인덱스
+        int inRoot = position[root]; //인오더의 루트 인덱스 : 루트를 중심으로 좌, 우 가 나눠짐
         int left = inRoot-is; //포스트오더의 왼쪽 자식의 수
-        solve(is, inRoot-1, ps, ps+left-1);
-        solve(inRoot+1, ie, ps+left, pe-1);
-    }
+        solve(is, inRoot-1, ps, ps+left-1); // 왼쪽 자식들 탐색
+        solve(inRoot+1, ie, ps+left, pe-1); // 오른쪽 자식들 탐색
+    } // 이렇게 탐색하다보면 루트 -> 좌 -> 우 순서대로 탐색하며 출력함
     
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();    
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();    
         
-        for(int i=0; i<n; i++)
-            inOrder[i] = scan.nextInt();
-        for(int i=0; i<n; i++)
-            postOrder[i] = scan.nextInt();
-        for(int i=0; i<n; i++)
-            position[inOrder[i]] = i;
+        for(int i=0; i<n; i++) inOrder[i] = sc.nextInt();
+        for(int i=0; i<n; i++) postOrder[i] = sc.nextInt(); // 일단 인오더와 포스트오더 입력 받고
+        for(int i=0; i<n; i++)  position[inOrder[i]] = i; // 인오더 왼쪽 오른쪽 나눠서 재귀탐색 해야 하므로 inOrder값이 들어있는 인덱스 i를 저장함
         
+        // 인오더, 포스트오더 모든 범위 집어넣어서 함수 계산
         solve(0, n-1, 0, n-1);
     }
  

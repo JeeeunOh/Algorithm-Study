61ac5129b9d4e58e90deca5d1000bbc834a7770c
0909oje <0909oje@naver.com>
2021-02-13 20:19:29 +0900

    다익스트라 실버문제

---
+++ ICPC_winter_2021/src/다익스트라/ex11286.java
@@ -0,0 +1,14 @@
+package 다익스트라;
+
+// 우선순위 큐라는 알고리즘이 사용됨
+// 일반적인 큐 : 먼저 들어온 데이터가 먼저 나감
+// 우선순위 큐는 우리가 정한 우선순위 높은 데이터가 먼저  나감
+
+import java.util.*;
+
+public class ex11286 { 
+	public static void main(String[] args) {
+
+	}
+
+}

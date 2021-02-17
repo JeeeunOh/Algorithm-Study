8f1097b84cf75a59a20e2472a3267fbb38000455
0909oje <0909oje@naver.com>
2021-02-17 16:34:11 +0900

    이독 주석달기

--- EDOC/src/다익스트라_플와_210217/ex11780.java
+++ EDOC/src/다익스트라_플와_210217/ex11780.java
@@ -1,70 +1,72 @@
 package 다익스트라_플와_210217;
 
-import java.io.*;
 import java.util.*;
 
 public class ex11780 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n =sc.nextInt();
-		int m = sc.nextInt();
+		int n = sc.nextInt(); // 도시 개수
+		int m = sc.nextInt(); // 버스 개수
 		int[][] d = new int [n + 1][n + 1];
 		int[][] next = new int[n + 1][n + 1];
-		final int INF = 10000001;
+		final int INF = 10000001; // 비용 최대치 설정해놓기
 		
 		for (int i = 1; i <= n; i++) {
-			Arrays.fill(d[i], INF);
-			d[i][i] = 0;
+			Arrays.fill(d[i], INF); // 한 마을에서 한 마을로 가는 비용 최대로 설정
+			d[i][i] = 0; // i에서 i로 가는 비용 0
 		}
 		
 		for (int i = 0; i < m; i ++) {
-			int start = sc.nextInt();
-			int end = sc.nextInt();
-			int dist = sc.nextInt();
-			
-			d[start][end] = Math.min(d[start][end], dist);
-			next[start][end] = end;
+			int start = sc.nextInt(),end = sc.nextInt(),cost = sc.nextInt();
+			// start 도시에서 end도시까지 드는 비용 cost
+			d[start][end] = Math.min(d[start][end], cost); //start에서 end로 가는 최소 비용 갱신
+			next[start][end] = end; 
 		}
 		
 		for (int k = 1; k <= n; k++) {
 			for (int i = 1; i <= n; i++) {
 				for (int j = 1; j <= n; j++) {
-					if (d[i][j] > d[i][k] + d[k][j]) {
-						d[i][j] = d[i][k] + d[k][j];
-						next[i][j] = next[i][k];
+					//직선으로 가는 경우가 없는경우, 우회해서 가는게 더 빠른경우 갱신
+					if (d[i][j] > d[i][k] + d[k][j]) { 
+						d[i][j] = d[i][k] + d[k][j]; 
+						next[i][j] = next[i][k]; 
 					}
 				}
 			}
 		}
 		
+		/*
+		그 다음에는 n×n개의 줄을 출력해야 한다.
+		 -> 그 다음, 도시 i에서 도시 j로 가는 경로를 공백으로 구분해 출력한다. 
+		  -> 이때, 도시 i와 도시 j도 출력해야 한다. 만약, i에서 j로 갈 수 없는 경우에는 0을 출력한다.
+		 */
+		
+		// 도시 i에서 j로 가는데 필요한 최소 비용 출력
+		// 처음 초기화 값 그대로 일 떄는 경로가 없다는 것이므로 0으로 바꿔준다.
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
-				if (d[i][j] == INF) {
-					d[i][j] = 0;
-				}
+				if (d[i][j] == INF)  d[i][j] = 0; 
 				System.out.print(d[i][j] + " ");
 			}
 			System.out.println();
-		}
+		} 
+		
+		 // i×n+j번째 줄에는 도시 i에서 도시 j로 가는 최소 비용에 포함되어 있는 도시의 개수 k를 출력한다. 
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
-				if (d[i][j] == 0) {
+				if (d[i][j] == 0) { // i에서 i로 가는 비용 x -> 0 출력
 					System.out.println("0");
 					continue;
 				}
 				ArrayList<Integer> path = new ArrayList<>();
-				int cur = i;
-				
-				while (cur != j) {
-					path.add(cur);
-					cur = next[cur][j];
+				int cur = i; // i부터 시작
+				while (cur != j) { // 끝 지점인 j에 도착하기 전까지
+					path.add(cur); // 현재 지점 추가하고
+					cur = next[cur][j]; // 다음 cur 지정
 				}
-				path.add(cur);
-				
-				System.out.print(path.size() + " ");
-				for (int p : path) {
-					System.out.print(p + " ");
-				}
+				path.add(cur); // 마지막 지점 마저 넣어주고
+				System.out.print(path.size() + " "); // 배열 사이즈 출력한 후
+				for (int p : path)  System.out.print(p + " ");  // 중간지점들 다 출력
 				System.out.println();
 			}
 		}
--- EDOC/src/다익스트라_플와_210217/ex1753.java
+++ EDOC/src/다익스트라_플와_210217/ex1753.java
@@ -1,16 +1,14 @@
 package 다익스트라_플와_210217;
 
-import java.io.*;
+// 최단경로
 import java.util.*;
 
 class Node implements Comparable <Node> {
       int end, weight;
-
       public Node (int end, int weight){
             this.end = end;
             this.weight = weight;
       }
-
       public int compareTo(Node o){ // 가중치 더 큰 것이 앞으로 온다.
     	  // 양수인 것이 앞에, 음수인 것은 뒤에
             return weight - o.weight;
@@ -22,31 +20,26 @@ public class ex1753 {
       static int v, e, k;
       static List<Node>[] list;
       static int[] dist;
-     
       public static void main(String[] args) {
     	  Scanner sc = new Scanner(System.in);
-            v = sc.nextInt();
-            e =sc.nextInt();
-            k =sc.nextInt();
-           
+            v = sc.nextInt(); // 정점 개수
+            e =sc.nextInt(); // 간선 개수
+            k =sc.nextInt(); // 시작 정점 번호
             list = new ArrayList[v+1];
-            dist = new int[v+1];
+            dist = new int[v+1]; // 최단경로값
            
-            Arrays.fill(dist, INF);
-           
-            for (int i=1; i <= v; i++){
-                list[i] = new ArrayList<>();
-            }
+            Arrays.fill(dist, INF); // 배열을 최대값으로 초기화해줌
             
+            // 해당 정점과 연결된 정점, 그 가중치를 저장하는 연결리스트 만들어준다.
+            for (int i=1; i <= v; i++) list[i] = new ArrayList<>(); 
+            
+            // 위 연결리스트의 값을 입력받아 저장한다.
             for(int i = 0; i < e; i++){
-                int start =sc.nextInt();
-                int end =sc.nextInt();
-                int weight = sc.nextInt();
+                int start =sc.nextInt(),end =sc.nextInt(),weight = sc.nextInt();
                 list[start].add(new Node(end, weight));
             }
-            
             dijkstra(k);
-           
+            
             for(int i=1; i <= v; i++){
                 if(dist[i] == INF) System.out.println("INF");
                 else System.out.println(dist[i]);
@@ -54,22 +47,21 @@ public class ex1753 {
       }
       
       private static void dijkstra(int start){
-            PriorityQueue<Node> queue = new PriorityQueue<>();
+            PriorityQueue<Node> q = new PriorityQueue<>();
             boolean[] check = new boolean[v+1];
-            queue.add(new Node(start, 0));
+            q.add(new Node(start, 0)); // 시작정점 집어넣고 연산 시작
             dist[start] = 0;
            
-            while(!queue.isEmpty()){
-            	Node curNode = queue.poll(); // 가중치가 작은것부터
+            while(!q.isEmpty()){
+            	Node curNode = q.poll(); // 가중치가 작은것부터
                int cur = curNode.end;
-               
-               if(check[cur] == true) continue;
-               check[cur] = true;
-               
-               for (Node node: list[cur]){
+               if(check[cur] == true) continue; // 방문한 적 있으면 넘기고
+               check[cur] = true; // 아니면 지금 방문하자
+               for (Node node: list[cur]){ // 현재 노드와 연결된 노드들 차례로 방문하기
+            	   // end노드 방문하는 가중치가 현재 노드 방문해서 가는 것보다 크면 새로 갱신해준다.
                     if(dist[node.end] > dist[cur] + node.weight){
                         dist[node.end] = dist[cur] + node.weight;
-                        queue.add(new Node(node.end, dist[node.end]));
+                        q.add(new Node(node.end, dist[node.end])); // 큐에다가 현재 노드 도착점과, 거기까지 가는데 걸린 가중치?저장
                     }
                }
             }    

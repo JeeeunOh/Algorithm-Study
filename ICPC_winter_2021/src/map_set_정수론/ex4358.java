3b27362db7849bd668ea3ad75e3aa367007c1b34
0909oje <0909oje@naver.com>
2021-02-18 19:55:52 +0900

    나무 입력 받고 -> 나무 분배비율 출력

--- ICPC_winter_2021/src/map_set_정수론/ex4358.java
+++ ICPC_winter_2021/src/map_set_정수론/ex4358.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.*;
 
 //문자열, 해시맵
-
 public class ex4358 {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

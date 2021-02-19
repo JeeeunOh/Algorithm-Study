6b5968b4b22316453879b45ba731b8da8d9743f4
0909oje <0909oje@naver.com>
2021-02-19 11:05:01 +0900

    정수론 어려워 ㅜㅜㅜ

--- ICPC_winter_2021/src/map_set_정수론/ex16563_sol.java
+++ ICPC_winter_2021/src/map_set_정수론/ex16563_sol.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ex16563_sol {
 	static int[] check;
 	static StringBuilder sb = new StringBuilder();
-	public static void factorSimple(int n) {
+	public static void Calc(int n) {
 		while(n>1) {
 			sb.append(check[n]+" ");
 			n /= check[n];
@@ -14,26 +14,22 @@ public class ex16563_sol {
 	}
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int[] num = new int[n];
-		for(int i=0; i<n; i++)
-			num[i] = s.nextInt();
+		int N = s.nextInt();
+		int[] k = new int[N];
+		for(int i=0; i<N; i++) k[i] = s.nextInt();
 		
 		check = new int[5000001];
 		check[0] = check[1] = -1;
-		for(int i=2; i<=5000000; i++)
-			check[i] = i;
+		for(int i=2; i<=5000000; i++) check[i] = i;
 		for(int i=2; i<=Math.sqrt(5000000); i++) {
 			if(check[i] == i) {
-				for(int j=i*2; j<=5000000; j+=i) {
-					if(check[j] == j)
-						check[j] = i;
-				}
+				for(int j=i*2; j<=5000000; j+=i)
+					if(check[j] == j) check[j] = i; 
 			}
 		}
-		
-		for(int i=0; i<n; i++)
-			factorSimple(num[i]);
+		for(int i=0; i<N; i++) Calc(k[i]);
 		System.out.println(sb);
 	}
 }
+
+
--- ICPC_winter_2021/src/map_set_정수론/ex20302_솔루션.java
+++ ICPC_winter_2021/src/map_set_정수론/ex20302_솔루션.java
@@ -5,16 +5,26 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+/*
+ * 1. 모든 합성수는 소인수분해를 통해 소수의 곱으로 표현할 수 있고, 
+ * 그 개수들을 카운트하기 위해 소수들의 개수를 담는 배열을 사용합니다. 
+ * 또한 정수, 유리수 판별에 음수 여부는 관계없으므로 편의를 위해 모두 양수로 바꿔줍니다.
+
+2. 곱하는 수는 소인수 분해하여 표현된 소수들의 개수를 올려주고, 
+나누는 수는 소인수 분해하여 표현된 소수들의 개수를 빼주어 그 개수들을 관리할 수 있습니다. 
+(곱하는 수 중 0이 존재한다면 항상 결과는 0이므로 이는 따로 처리해줍니다.)
+
+3. 모든 수를 소인수분해하고나서 구간[2, 1e5] 소수 배열을 순회하는데 
+그 값이 음수인 경우 분모에 수가 남게 되는 것이므로 이는 유리수라고 판정할 수 있고, 
+그 외는 정수라고 출력하면 됩니다.
+ */
 
 public class ex20302_솔루션{
     private static int[] head = new int[100_001];
     private static int[] tail = new int[100_001];
-
     private static ArrayList<Integer> primes = new ArrayList<>();
     private static boolean[] prime = new boolean[100_001];
-
     private static boolean zeroes;
-
     private static final char MUL = '*';
 
     public static void main(String[] args) throws Exception {

e42bf79cfb27ef09a45d33bf6b46b6bf79c4285a
0909oje <0909oje@naver.com>
2021-02-27 15:39:12 +0900

    두 용액, 투포인터

--- ICPC_winter_2021/src/정렬_이분탐색/ex2470.java
+++ ICPC_winter_2021/src/정렬_이분탐색/ex2470.java
@@ -1,6 +1,6 @@
 package 정렬_이분탐색;
 // dp, 그리디, 이분탐색, 투포인터 사용 가능 -> 여기서는 투포인터
-
+// 두 용액
 import java.util.*;
 import java.io.*;
 
@@ -18,13 +18,13 @@ public class ex2470 {
 		long result = Integer.MAX_VALUE;
 		int answer1=0,answer2=0;
 		
-		while(start!=end) {
+		while(start!=end) { // 투포인터에서는 항상 조건 설정 중요함
 			if(result>Math.abs(A[end]+A[start])) {
 				result=Math.abs(A[end]+A[start]);
 				answer1=A[start];
 				answer2=A[end];
 			}
-			if(Math.abs(A[start]+A[end-1])<Math.abs (A[start+1] + A[end])) end--;
+			if(Math.abs(A[start]+A[end-1])<Math.abs (A[start+1] + A[end])) end--; // 시작+(끝-1) < (시작+1) 끝 -> 끝--;
 			else start++;
 		}
 		System.out.println(answer1 + " " + answer2);

38fbd95ce3e70d147edf7cf5f8eb6e5f2cd271bc
0909oje <0909oje@naver.com>
2021-03-22 19:49:14 +0900

    이독 문제풀이

---
+++ EDOC/src/백트래킹/ex14888.java
@@ -0,0 +1,10 @@
+package 백트래킹;
+
+public class ex14888 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ EDOC/src/백트래킹/ex17136.java
@@ -0,0 +1,10 @@
+package 백트래킹;
+
+public class ex17136 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

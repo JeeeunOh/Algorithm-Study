b2995be6f205f2f4896c1172bcf919cef22f5ee6
0909oje <0909oje@naver.com>
2021-03-23 12:29:33 +0900

    이독 중 문제

--- EDOC/src/백트래킹/ex14888.java
+++ EDOC/src/백트래킹/ex14888.java
@@ -1,10 +1,27 @@
 package 백트래킹;
 
+import java.util.*;
 public class ex14888 {
-
+	static int[] num, op;
+	static int n, min= 1000000000, max=-1000000000;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		num = new int[n]; // 연산숫자 저장
+		for(int i=0 ; i<n ; i++) num[i] = sc.nextInt();
+		
+		op = new int[4]; // 연산자 개수 저장
+		for (int i=0 ; i<n ; i++)  op[i] = sc.nextInt();
+		
+		calc(0,0); // 현재 진행된 절차 0,  연산중간결과 0
+		
+		System.out.println(max);
+		System.out.println(min);
 	}
-
+	public static int calc(int idx, int temp) {
+		int res=0;
+		
+		return res;
+	}
 }

612c26ba8fe00e97379a6d6961a8c278149075e6
0909oje <0909oje@naver.com>
2021-04-07 18:51:11 +0900

    올바른 문자열을 만들기 위한 cnt

--- EDOC/src/그리디/boj4889.java
+++ EDOC/src/그리디/boj4889.java
@@ -22,11 +22,10 @@ public class boj4889 {
                     stack.push(d); //스택에 집어넣는다
                     continue;
                 }
-                // d=='}'이고
-                if(stack.isEmpty()){ //스택이 비어있다면
+                if(stack.isEmpty()){ // d=='}'이고, 스택이 비어있다면
                     stack.push('{'); // }를 {로 바꾼것을 집어넣는다.
                     cnt++; 
-                } else stack.pop(); // 스택 비어있지 않다면 앞은 {일 것이므로 빼낸다. { } 한쌍을 처리한것과 같다.
+                } else stack.pop(); // d=='}'이고, 스택 비어있지 않다면 앞은 {일 것이므로 빼낸다. { } 한쌍을 처리한것과 같다.
             }
             cnt += (stack.size() / 2);
 

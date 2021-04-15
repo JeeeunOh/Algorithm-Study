4bd7a83f4d8bcc2174674e701b26d84de87d016d
0909oje <0909oje@naver.com>
2021-04-15 23:42:20 +0900

    폰커밋

--- EDOC/src/그리디/boj1071.java
+++ EDOC/src/그리디/boj1071.java
@@ -23,12 +23,11 @@ public class boj1071 {
 			if (arr[i + 1] == arr[i] + 1) { // arr[i]과  arr[i+1]이 연속된 수 일때
 				// arr[i], arr[i+1]을 비교했으므로, 문제의 조건에 위배되지 않는 arr[k]값을 찾기 위해 그 다음 index부터 탐색
 				// 그 전 index부터 탐색하면 사전순으로 가장 앞에 있는 경우에 위배되므로 뒤부터 탐색함
-				for (k = i + 2; k < n; k++) if (arr[k] != arr[i + 1]) break; // arr[i]+1과 같지 않은 수 arr[k]를 찾고 반복문 종료
+				for (k = i + 2; k < n; k++) if (arr[k] != arr[i + 1]) break; // arr[i]+1과 같지 않은 수 arr[k]를 찾고 반복문 종료한다
 				if (k == n) { //k==n이라는 것은 기준점 i의 뒤에서 문제의 조건을 만족하는 k찾지 못한 것임 arr[i]+1과 같지 않은 수가 없다면
 					for (k = i; k >= 0; k--) if (arr[k] != arr[i]) break; //i의 앞을 탐색해서 현재수와 같지 않은 수를 찾는다
 					swap(i+1, k + 1); // 찾아낸 수를 서로 바꿈
-				}
-				else swap(i+1, k); // k!=n이면 
+				}else swap(i+1, k); // k!=n이면 
 			}
 		}
 	

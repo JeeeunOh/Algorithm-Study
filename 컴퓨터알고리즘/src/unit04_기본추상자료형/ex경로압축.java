e122da7f51ec0fe34b18e2f390191462713b595a
0909oje <0909oje@naver.com>
2021-04-21 23:47:48 +0900

    틀 짜놓기

--- 컴퓨터알고리즘/src/unit04_기본추상자료형/ex경로압축.java
+++ 컴퓨터알고리즘/src/unit04_기본추상자료형/ex경로압축.java
@@ -1,6 +1,6 @@
 package unit04_기본추상자료형;
-// 구현 해주심
-public class ex기간 {
+
+public class ex경로압축 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- 컴퓨터알고리즘/src/unit04_기본추상자료형/ex정수범위로매핑가능한집합.java
+++ 컴퓨터알고리즘/src/unit04_기본추상자료형/ex정수범위로매핑가능한집합.java
@@ -1,5 +1,5 @@
 package unit04_기본추상자료형;
-
+// 구현안해주심
 public class ex정수범위로매핑가능한집합 {
 
 	public static void main(String[] args) {

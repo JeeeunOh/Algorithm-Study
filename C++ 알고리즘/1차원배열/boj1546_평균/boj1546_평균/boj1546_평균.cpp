840029829170f250725806abaa1a17ba06af168c
0909oje <0909oje@naver.com>
2021-08-19 13:49:17 +0900

    일차원배열

---
+++ C++ 알고리즘/1차원배열/boj1546_평균/boj1546_평균/boj1546_평균.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n=0;
+    int max = 0;
+    cin >> n;
+    float arr[1000];
+    float sum = 0;
+    float avg;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (max < arr[i]) max = arr[i];
+        sum += arr[i];
+    }
+    avg = (sum / max * 100) / n;
+
+    cout << fixed;
+    cout.precision(6);
+    cout << avg << endl;
+}

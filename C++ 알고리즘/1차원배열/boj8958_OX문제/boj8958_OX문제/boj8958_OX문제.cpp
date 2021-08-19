89b40156643f1564e4d2f2992154626be3a0d91a
0909oje <0909oje@naver.com>
2021-08-19 14:38:08 +0900

    일차원배열

---
+++ C++ 알고리즘/1차원배열/boj8958_OX문제/boj8958_OX문제/boj8958_OX문제.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int p;
+        cin >> p;
+        float arr[1000];
+        float sum = 0, cnt = 0;
+        for (int i = 0; i < p; i++) {
+            cin >> arr[i];
+            sum += arr[i];
+        }
+        sum /= p;
+        
+        for (int i = 0; i < p; i++) {
+            if (arr[i] > sum) cnt++;
+        }
+        float ans = cnt / p *100;
+
+        cout << fixed;
+        cout.precision(3);
+        cout << ans <<"%"<<  endl;
+    }
+}

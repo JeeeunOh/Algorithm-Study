406c0c9d0b4e814baf09f902ad17791c0833a77b
0909oje <0909oje@naver.com>
2021-08-21 13:05:59 +0900

    그룹단어체커

---
+++ C++ 알고리즘/문자열/boj1316_그룹단어체커/boj1316_그룹단어체커/boj1316_그룹단어체커.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+    int i, cnt = 0;
+    char s[101];
+    scanf("%d", &i);
+    while (i--) {
+        scanf("%s", s);
+        int len = strlen(s);
+        len = unique(s, s + len) - s;
+        sort(s, s + len);
+        if (unique(s, s + len) == s + len) cnt++;
+    }
+    printf("%d", cnt);
+    return 0;
+}

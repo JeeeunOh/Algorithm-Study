18caa4b5c208fb46db9699f0c1a934627e5110fe
0909oje <0909oje@naver.com>
2021-08-22 16:37:19 +0900

    0으로 나누는것에 유의

---
+++ C++ 알고리즘/기본수학1/boj1712_손익분기점/boj1712_손익분기점/boj1712_손익분기점.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	// a는 고정비용, b는 생산비용, c는 측정된 노트북 가격
+	// (c-b)*ans > a
+	// ans> a/(c-b)
+
+	if (c <= b) {
+		cout << "-1" << endl;
+		return 0;
+	} 
+	else {
+		int ans = a / (c - b) + 1;
+		cout << ans << endl;
+	}
+}

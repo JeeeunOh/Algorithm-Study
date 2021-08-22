77fb65c1cf1057200cfad94e196c0b65d805c49d
0909oje <0909oje@naver.com>
2021-08-22 17:05:59 +0900

    논리적사고0 , not 코딩실력

---
+++ C++ 알고리즘/기본수학1/boj2869_달팽이는올라가고싶다/boj2869_달팽이는올라가고싶다/boj2869_달팽이는올라가고싶다.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, v;
+	cin >> a >> b >> v;
+	int i = 0; 
+	int cnt = 0;
+	
+	if ((v - a) % (a - b) == 0) cnt = (v - a) / (a - b);
+	else cnt = (v - a) / (a - b) + 1;
+	cout << cnt+1 << endl;
+}

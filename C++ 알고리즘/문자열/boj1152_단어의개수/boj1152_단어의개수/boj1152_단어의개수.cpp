b17c09b4548fcf155379bd7ec203c9fcbbac4c29
0909oje <0909oje@naver.com>
2021-08-22 16:23:52 +0900

    문자열_문자의개수

---
+++ C++ 알고리즘/문자열/boj1152_단어의개수/boj1152_단어의개수/boj1152_단어의개수.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    int cnt = 1;
+    getline(cin, s);
+    if (s.empty()) {
+        cout << "0";
+        return 0;
+    }
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') cnt++;
+    }
+    if (s[0] == ' ') cnt--;
+    if (s[s.length() - 1] == ' ') cnt--;
+    cout << cnt << endl;
+}

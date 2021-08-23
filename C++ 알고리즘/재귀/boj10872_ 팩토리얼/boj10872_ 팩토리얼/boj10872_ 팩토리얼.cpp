adb697d8dc6282080a9eed3e12cce6da58f62c06
0909oje <0909oje@naver.com>
2021-08-23 16:20:32 +0900

    재귀 시작

---
+++ C++ 알고리즘/재귀/boj10872_ 팩토리얼/boj10872_ 팩토리얼/boj10872_ 팩토리얼.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int result = 1;
+    if (n == 0) {
+        cout << 1;
+        return 0;
+    }
+    else {
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+    }
+    cout << result;
+    return 0;
+}

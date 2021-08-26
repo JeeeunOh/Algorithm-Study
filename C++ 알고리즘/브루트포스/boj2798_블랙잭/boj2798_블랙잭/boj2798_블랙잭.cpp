34432aafe92b4e3e8055ed37b0bab37677c344f0
0909oje <0909oje@naver.com>
2021-08-26 13:40:26 +0900

    삼중 for문

---
+++ C++ 알고리즘/브루트포스/boj2798_블랙잭/boj2798_블랙잭/boj2798_블랙잭.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	int arr[100] = { 0,};
+	int max = 0;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	for(int i=0 ; i< n-2; i++)
+		for (int j = 0; j < n - 1; j++) {
+			for (int k = 0; k < n; k++) {
+				if (i == j || j == k || i == k) continue;
+				if (arr[i] + arr[j] + arr[k] > max && arr[i] + arr[j] + arr[k] <= m)
+				{
+					max = arr[i] + arr[j] + arr[k];
+				}
+			}
+		}
+	cout << max;
+}

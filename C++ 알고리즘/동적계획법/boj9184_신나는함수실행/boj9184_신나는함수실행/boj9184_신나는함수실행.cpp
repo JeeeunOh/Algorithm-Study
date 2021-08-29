dbe720835d6495eb60898c05b4c48bf9efd4614e
0909oje <0909oje@naver.com>
2021-08-29 23:43:39 +0900

    dp 동적계획법

---
+++ C++ 알고리즘/동적계획법/boj9184_신나는함수실행/boj9184_신나는함수실행/boj9184_신나는함수실행.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;
+
+int a, b, c;
+int dp[21][21][21];
+
+int w(int a, int b, int c)
+{
+	if (a <= 0 || b <= 0 || c <= 0) return 1;
+	else if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+	else if (a < b && b < c)
+	{
+		if (dp[a][b][c] != 0) return dp[a][b][c];
+		else return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+	}
+	else
+	{
+		if (dp[a][b][c] != 0) return dp[a][b][c];
+		else return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+	}
+}
+
+int main()
+{
+	while (1)
+	{
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1) break;
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << endl;
+	}
+
+}

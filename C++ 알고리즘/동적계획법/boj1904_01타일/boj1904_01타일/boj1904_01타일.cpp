ef61cf97e4dba20158ad4f4fc133b1e887b18960
0909oje <0909oje@naver.com>
2021-08-31 18:15:12 +0900

    피보나치 동적계획법

---
+++ C++ 알고리즘/동적계획법/boj1904_01타일/boj1904_01타일/boj1904_01타일.cpp
@@ -0,0 +1,25 @@
+﻿#include <cstdio>
+#include <vector>
+using namespace std;
+
+int N;
+vector<long long> answer = { 0,1,2 };
+
+void find_answer()
+{
+    long long tmp;
+    for (int i = 3; i <= N; i++)
+    {
+        tmp = 0;
+        tmp = answer[i - 1] + answer[i - 2];
+        answer.push_back(tmp % 15746);
+    }
+}
+
+int main() {
+    scanf("%d", &N);
+    find_answer();
+    printf("%lld", answer[N] % 15746);
+}
+
+// push back pop back

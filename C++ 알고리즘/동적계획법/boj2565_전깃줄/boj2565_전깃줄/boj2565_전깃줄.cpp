fb49c2dfe4559d168ee3677bbe32a2737c0df524
0909oje <0909oje@naver.com>
2021-08-31 19:35:30 +0900

    꼬인전깃줄을 풀어보자

---
+++ C++ 알고리즘/동적계획법/boj2565_전깃줄/boj2565_전깃줄/boj2565_전깃줄.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#define endl "\n"
+#define MAX 110
+using namespace std;
+
+struct LINE
+{
+    int Left;
+    int Right;
+};
+
+int N;
+int DP[MAX];
+LINE Line[MAX];
+
+int Max(int A, int B) { if (A > B) return A; return B; }
+
+void Input()
+{
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> Line[i].Left >> Line[i].Right;
+    }
+}
+
+bool Cmp(LINE A, LINE B)
+{
+    if (A.Left < B.Left) return true;
+    return false;
+}
+
+void Solution()
+{
+    int Correct_Line = 0;
+    sort(Line + 1, Line + N + 1, Cmp);
+    for (int i = 1; i <= N; i++)
+    {
+        DP[i] = 1;
+        for (int j = 1; j < i; j++)
+        {
+            if (Line[i].Right > Line[j].Right)
+            {
+                DP[i] = Max(DP[i], DP[j] + 1);
+            }
+        }
+        Correct_Line = Max(Correct_Line, DP[i]);
+    }
+    cout << N - Correct_Line << endl;
+}
+
+void Solve()
+{
+    Input();
+    Solution();
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    //freopen("Input.txt", "r", stdin);
+    Solve();
+
+    return 0;
+}
+
+
+출처: https://yabmoons.tistory.com/572 [얍문's Coding World..]

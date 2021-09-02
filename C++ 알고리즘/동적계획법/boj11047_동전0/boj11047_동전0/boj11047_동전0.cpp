730217cf64876f6a7cfa4082f4122b1265bc1a6c
0909oje <0909oje@naver.com>
2021-09-02 18:47:18 +0900

    동전개수 구하는 그리디

---
+++ C++ 알고리즘/동적계획법/boj11047_동전0/boj11047_동전0/boj11047_동전0.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	int arr[10];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		if (arr[i] > k) {
+			i--;
+			n--;
+		}
+	}
+
+	int temp = n - 1;
+	int ans = 0;
+
+	while (k != 0) {
+		if (arr[temp] <= k) {
+			k -= arr[temp];
+			ans++;
+		}
+		else temp--;
+	}
+
+	cout << ans << endl;
+
+}

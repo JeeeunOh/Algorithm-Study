f6bd8eea4a6994c3e1992e6e4f87b5cc68871936
0909oje <0909oje@naver.com>
2021-09-02 20:40:55 +0900

    그리디

---
+++ C++ 알고리즘/그리디/boj1541_잃어버린괄호/boj1541_잃어버린괄호/boj1541_잃어버린괄호.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+
+/*
+stoi = string to int
+stof = string to float
+stol = string to long
+stod = string to double
+*/
+
+using namespace std;
+
+string str;
+
+int minResult(void){
+
+    int result = 0;
+    string temp = ""; // temp는 숫자 모음
+    bool minus = false;
+
+    for (int i = 0; i <= str.size(); i++){
+        //연산자일 경우
+        if (str[i] == '+' || str[i] == '-' || str[i] == '\0') {
+
+            if (minus) result -= stoi(temp); // 앞에 마이너스가 있을경우
+            else result += stoi(temp); // 앞이 플러스일 경우 더해줌
+
+            temp = ""; //초기화
+
+            //괄호를 뺄셈 이후에 치면 최소
+
+            if (str[i] == '-') minus = true; // 앞에 마이너스 있으면 마이너스 bol을 true로 만들어줌
+
+            continue;
+
+        }
+        //피연산자일 경우
+        temp += str[i];
+    }
+    return result;
+}
+
+
+int main(void){
+    cin >> str; // str받고
+    cout << minResult() << endl; // 계산함
+    return 0;
+}

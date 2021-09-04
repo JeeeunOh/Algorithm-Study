72caec44ab52d78d2288f9027c502ea4dde5aa52
0909oje <0909oje@naver.com>
2021-09-04 20:54:53 +0900

    스택 시작

---
+++ C++ 알고리즘/정수론 및 조합론/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+	int tot; // 약수의 총 개수
+
+	cin >> tot;
+	int* div = new int[tot]; // 모든 약수
+
+	for (int i = 0; i < tot; i++)
+	{
+		cin >> div[i];
+	}
+
+	sort(div, div + tot);
+
+	cout << div[0] * div[tot - 1];
+
+}

7311bdd8836e05109f64f0772966fde3d27fd976
0909oje <0909oje@naver.com>
2021-09-05 22:09:39 +0900

    기본 스택 문제

--- C++ 알고리즘/스택/boj10773_제로/boj10773_제로/boj10773_제로.cpp
+++ C++ 알고리즘/스택/boj10773_제로/boj10773_제로/boj10773_제로.cpp
@@ -1,6 +1,29 @@
 ﻿#include <iostream>
+#include <cstring>
+#include <stack>
+#include <string>
+#include <algorithm>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int sum = 0;
+	int num;
+	int N;
+	cin >> N;
+	stack<int> s;
+
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		if (num != 0) s.push(num);
+		else s.pop();
+	}
+
+	for (int i = 0; !s.empty(); i++) {
+		sum += s.top();
+		s.pop();
+	}
+	cout << sum << endl;
+
 }

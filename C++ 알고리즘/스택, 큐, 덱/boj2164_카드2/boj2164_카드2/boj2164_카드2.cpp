75de7ccb91f1891d0f98472cdc1752fa894b08af
0909oje <0909oje@naver.com>
2021-09-06 16:05:21 +0900

    큐 기본 문제

---
+++ C++ 알고리즘/스택, 큐, 덱/boj2164_카드2/boj2164_카드2/boj2164_카드2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	queue<int> q;
+
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+	for (int i = 0; q.size()>1; i++) {
+		q.pop(); // 제일 위 카드 하나 버림
+		q.push(q.front()); // 제일 밑에 집어넣음
+		q.pop(); // 집어넣었으니 제일 위 것은 버림
+	}
+	cout << q.front();
+}
+
+// 1 2 3 4 5 6

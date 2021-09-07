87e2b58537fcc3a86176a8ea3d4592df210c5a97
0909oje <0909oje@naver.com>
2021-09-08 00:12:54 +0900

    시험감독 구현

---
+++ C++ 알고리즘/EPPER/boj13458_시험감독/boj13458_시험감독/boj13458_시험감독.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; // 시험장 개수
+    int b; // 총감독관이 한 시험장에서 감시할 수 있는 응시자의 수
+    int c; // 부 감독관이 감시 ~
+    int arr[1000000]; // 각 시험장 응시자 수
+
+
+    std::cout << "Hello World!\n";
+}

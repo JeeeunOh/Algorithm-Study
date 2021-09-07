9143e4511deb23a372863127b529f3fe900e04ff
0909oje <0909oje@naver.com>
2021-09-07 23:30:18 +0900

    구현문제

---
+++ C++ 알고리즘/EPPER/boj1259_팰린드롬수/boj1259_팰린드롬수/boj1259_팰린드롬수.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input;
+	while (true) {
+		cin >> input;
+		int flag = 1; // yes로 시작
+		if (input=="0") break;
+		for (int i = 0; i < input.size() / 2; i++) { //6이면 0 1 2, 5면 0 1
+			if (input[i] != input[input.size() - i - 1]) {
+				cout << "no" << endl;
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1) cout << "yes" << endl;
+	}
+}

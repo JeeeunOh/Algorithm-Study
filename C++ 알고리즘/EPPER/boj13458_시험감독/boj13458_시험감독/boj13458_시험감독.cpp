d48dc7269062f06d7f30dee6bcbab5d23bf11017
0909oje <0909oje@naver.com>
2021-09-08 14:56:34 +0900

    벡터 사용 브론즈 문제

--- C++ 알고리즘/EPPER/boj13458_시험감독/boj13458_시험감독/boj13458_시험감독.cpp
+++ C++ 알고리즘/EPPER/boj13458_시험감독/boj13458_시험감독/boj13458_시험감독.cpp
@@ -1,14 +1,32 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-    int n; // 시험장 개수
-    int b; // 총감독관이 한 시험장에서 감시할 수 있는 응시자의 수
-    int c; // 부 감독관이 감시 ~
-    int arr[1000000]; // 각 시험장 응시자 수
+int main(){
+    long long N; // 시험장 개수
+    long long B; // 총감독관이 한 시험장에서 감시할 수 있는 응시자의 수
+    long long C; // 부 감독관이 감시 ~
+    vector<int> v;
+    long long cnt = 0;
 
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int n;
+        cin >> n;
+        v.push_back(n);
+    }
+    cin >> B >> C;
 
-    std::cout << "Hello World!\n";
+    for (int i = 0; i < N; i++) {
+        v[i] -= B;
+        cnt++;
+        if (v[i] > 0) {
+            if (v[i] % C == 0) cnt += v[i] / C;
+            else cnt += v[i] / C + 1;
+        }
+    }
+
+    cout << cnt;
 }
+

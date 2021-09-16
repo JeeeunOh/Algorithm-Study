71e0265e86efe9abf56900c12955c4aad148ca70
0909oje <0909oje@naver.com>
2021-09-16 22:46:46 +0900

    사다리 문제

---
+++ C++ 알고리즘/EPPER/사다리/사다리/사다리.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+long long solution(int n, vector<int> goal) {
+	int ans = 0;
+
+	for (int i = 1; i < n; i++) {
+		for (int j = i-1; j >=0; j--) { // j는 i바로 전부터 탐색해서 0번까지 내림차순
+			if (goal[j] > goal[i]) ans++; // goal은 오름차순이어야하니, 내림차순이면
+			//ans++
+		}
+	}
+
+	return ans;
+}

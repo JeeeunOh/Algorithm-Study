73a32f7353e488bb6ca995f915b2e8efa85555f4
0909oje <0909oje@naver.com>
2021-09-17 22:42:23 +0900

    중 문제까지 완료

---
+++ C++ 알고리즘/EPPER/입국심사/입국심사/입국심사.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+	long long answer = LLONG_MAX;
+	sort(times.begin(), times.end());
+	long long start = 1;
+	long long end = (long long)n * (long long)times[times.size() - 1];
+	long long mid, temp;
+	while (start < end) {
+		mid = (start + end) / 2;
+		temp = 0;
+		for (int i = 0; i < times.size(); i++) {
+			temp += mid / times[i];
+		}
+		if (temp >= n) {
+			answer += min(answer, mid);
+			end = mid - 1;
+		}
+		else start = mid + 1;
+	}
+
+	return answer;
+}

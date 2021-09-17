27215750482a5efb06d2a4192887cedefebaf59a
0909oje <0909oje@naver.com>
2021-09-17 18:36:38 +0900

    신문나라

---
+++ C++ 알고리즘/EPPER/신문기사/신문기사/신문기사.cpp
@@ -0,0 +1,15 @@
+﻿#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> solution(int r, int c, int zr, int zc, vector<string> words){
+	vector<string> answer;
+	string temp; // 한 행씩 저장할 때, 사용할 임시 변수
+
+	
+	return answer;
+
+
+
+
+}
---
+++ C++ 알고리즘/EPPER/신문기사2/신문기사2/신문기사2.cpp
@@ -0,0 +1,20 @@
+﻿#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> solution(int r, int c, int zr, int zc, vector<string> words) {
+	vector<string> answer;
+	string temp;
+	for (int i = 0; i < r; i++) {
+		temp = "";
+		for (int j = 0; j < c; j++) {
+			for (int k = 0; k < zc; k++) {
+				temp += words[i][j];
+			}
+		}
+		for (int j = 0; j < zr; j++) {
+			answer.push_back(temp);
+		}
+	}
+	return answer;
+}

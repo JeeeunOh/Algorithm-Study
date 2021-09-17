354e5c7414ae875a5724968daedf9e1efd758b24
0909oje <0909oje@naver.com>
2021-09-17 18:56:51 +0900

    펠린드롬수

---
+++ C++ 알고리즘/EPPER/펠린드롬수/펠린드롬수/펠린드롬수.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+bool solution(int input) {
+	int original = input;
+	int reverse = 0;
+
+	while (input > 0) {
+		reverse = reverse * 10 + input % 10;
+		input /= 10;
+	}
+
+	return original == reverse;
+
+}

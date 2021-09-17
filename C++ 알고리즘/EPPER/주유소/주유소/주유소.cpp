9431e817fd9b21d1e384f997fda42c1d349d7582
0909oje <0909oje@naver.com>
2021-09-17 21:57:15 +0900

    주유소

---
+++ C++ 알고리즘/EPPER/주유소/주유소/주유소.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+long long solution(int n, vector<int> dist, vector<int> price) {
+	// 도시 수, 도로 길이, 도로 리터당 가격, 
+	long long ans = 0;
+	long long min = price[0];
+	for (int i = 0; i < n - 1; i++) {
+		if (min > price[i]) min = price[i];
+		ans += min * dist[i];
+	}
+	return ans;
+}

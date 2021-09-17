5c27ce0297bfbc1c96fc869ab17296571f5c11e8
0909oje <0909oje@naver.com>
2021-09-17 19:53:09 +0900

    중문제 힘드러

---
+++ C++ 알고리즘/EPPER/문자열압축/문자열압축/문자열압축.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> solution(vector<string> s) {
+	vector<string> ans;
+	int cnt = 0;
+	if (s[0] == "1") ans.push_back("1");
+	for (int i = 0; i < s.size()-1; i++) {
+		if (s[i] == s[i + 1]) cnt++;
+		else {
+			ans.push_back("A" + cnt);
+			cnt = 0;
+		}
+	}
+	ans.push_back("A" + cnt);
+	return ans;
+}
+

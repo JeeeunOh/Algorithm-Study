f0ef129e9db4f614f9acd5c48f202e4b75d0b179
0909oje <0909oje@naver.com>
2021-09-17 18:43:04 +0900

    오엑스퀴즈

---
+++ C++ 알고리즘/EPPER/OX/OX/OX.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int solution(char str[]) {
+	int total = 0, score = 0;
+	int len = strlen(str);
+
+	for (int i = 0; i < len; i++) {
+		if (str[i] == 'O') score++;
+		else score = 0;
+		total += score;
+	}
+
+	return total;
+}

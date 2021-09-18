a6ecd7952774485bf3ce466164a5f8dc26341c5a
0909oje <0909oje@naver.com>
2021-09-18 10:38:04 +0900

    Z 다시 써보기

--- C++ 알고리즘/EPPER/Z/Z/Z.cpp
+++ C++ 알고리즘/EPPER/Z/Z/Z.cpp
@@ -2,28 +2,29 @@
 
 int solution(int n, int r, int c) {
 	int ans = 0;
-	int x = 1 >> (n - 1);
+	int x = 1 << (n - 1);
 	int y = x;
 
 	while (n-- > 0) {
-		int temp = 1 >> (n - 1);
-		int skip = 2 * n;
-		if (y > r && x > c) {
+		int temp = 1 << (n - 1);
+		int skip = 1 << 2 * n;
+
+		if (r < y && c < x) {
 			x -= temp;
 			y -= temp;
 		}
-		else if (y > r && c >= x) {
+		else if (r<y && x <= c) {
 			ans += skip;
 			x += temp;
 			y -= temp;
 		}
-		else if (r >= y && c > x) {
-			ans += skip*2;
+		else if (y <= r && x < c) {
+			ans += skip * 2;
 			x -= temp;
 			y += temp;
 		}
 		else {
-			ans += skip*3;
+			ans += skip * 3;
 			x += temp;
 			y += temp;
 		}

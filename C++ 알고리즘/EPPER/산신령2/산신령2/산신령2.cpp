7925a20dab202292ee6b76a2ed31cca75c0cdcb2
0909oje <0909oje@naver.com>
2021-09-18 10:56:48 +0900

    산신령

---
+++ C++ 알고리즘/EPPER/산신령2/산신령2/산신령2.cpp
@@ -0,0 +1,19 @@
+﻿#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+long long solution(int N, vector<vector<long long>> money) {
+	for (int i = 1; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			if (j == 0) money[i][0] += money[i - 1][0];
+			else if (j == i) money[i][j] += max(money[i-1][j], money[i - 1][j - 1]);
+			else money[i][j] += money[i - 1][j];
+		}
+	}
+	long long ans =money[N - 1][0];
+	for (int i = 0; i < N; i++) {
+		ans = max(money[N - 1][i], ans);
+	}
+	return ans;
+}
--- C++ 알고리즘/EPPER/입국심사/입국심사/입국심사.cpp
+++ C++ 알고리즘/EPPER/입국심사/입국심사/입국심사.cpp
@@ -1,27 +1,26 @@
-﻿#include <string>
-#include <vector>
+﻿#include <vector>
 #include <algorithm>
 #include <climits>
+
 using namespace std;
 
 long long solution(int n, vector<int> times) {
-	long long answer = LLONG_MAX;
+	long long ans = LLONG_MAX;
 	sort(times.begin(), times.end());
 	long long start = 1;
 	long long end = (long long)n * (long long)times[times.size() - 1];
-	long long mid, temp;
-	while (start < end) {
+	long long mid = 0;
+	while (start <= end) {
+		int temp = 0;
 		mid = (start + end) / 2;
-		temp = 0;
 		for (int i = 0; i < times.size(); i++) {
 			temp += mid / times[i];
 		}
 		if (temp >= n) {
-			answer += min(answer, mid);
+			ans = min(ans, mid);
 			end = mid - 1;
 		}
 		else start = mid + 1;
 	}
-
-	return answer;
+	return ans;
 }

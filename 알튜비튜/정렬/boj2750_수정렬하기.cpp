d99b439449cd8608d9393e773a31019df2ede922
Jieun Oh <0909oje@naver.com>
2022-01-14 00:24:48 +0900

    수 정렬하기 성공

--- 알튜비튜/정렬/boj2750_수정렬하기.cpp
+++ 알튜비튜/정렬/boj2750_수정렬하기.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
 int main(){
+    int N;
+    cin >> N;
+    int arr[1000];
+    for(int i=0; i<N ; i++) {
+        cin>>arr[i];
+    }
+    sort(arr,arr+N);
 
-
+    for(int i=0; i<N ; i++) {
+        cout<<arr[i]<<endl;
+    }
 }

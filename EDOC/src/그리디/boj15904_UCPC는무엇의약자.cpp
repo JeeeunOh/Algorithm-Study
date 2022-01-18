6fd7ee6e776420f98780c0536deb89f6e684d182
Jieun Oh <0909oje@naver.com>
2022-01-18 18:19:21 +0900

    cin으로 받을 시 띄어쓰기 포함 안됨에 유의하기

--- EDOC/src/그리디/boj15904_UCPC는무엇의약자.cpp
+++ EDOC/src/그리디/boj15904_UCPC는무엇의약자.cpp
@@ -1,4 +1,22 @@
-//
-// Created by 오지은 on 2022/01/18.
-//
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+int main(){
+    char input[1000];
+    scanf("%[^\n]s", input);
+
+    char ans[] = {"UCPC"};
+    int j=0; // UCPC 의 0 index부터 탐색해보자
+    int cnt=0;
+
+    for(int i=0 ; i<strlen(input); i++){
+        if(input[i]==ans[j]){
+            j++; cnt++;
+        }
+    }
+    if(cnt==4) cout << "I love UCPC" << endl;
+    else cout << "I hate UCPC" << endl;
+
+    return 0;
+}

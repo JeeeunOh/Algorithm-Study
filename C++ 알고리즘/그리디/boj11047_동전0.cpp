cd475d837a1398af818bb229617542b875808f27
Jieun Oh <0909oje@naver.com>
2022-01-25 17:07:36 +0900

    쉬운 그리디

--- C++ 알고리즘/그리디/boj11047_동전0.cpp
+++ C++ 알고리즘/그리디/boj11047_동전0.cpp
@@ -2,24 +2,20 @@
 using namespace std;
 
 int main(){
-    int arr[1000][2]; // 0: 입력받은배열 , 1:현재까지 증가수열 길이 최대값
-    int n;
-    int max=1; // 증가수열 길이 최대값 1로 초기화
-    cin >> n;
+    int n,k;
+    int arr[10];
+    cin >> n >> k;
+    for(int i=0 ; i<n ; i++) cin >> arr[i];
 
-    for(int i=0 ; i<n; i++){
-        cin >> arr[i][0]; // 배열 입력받기
-        arr[i][1]=1;
-        if(i>=1){// 두번째 입력 받은 수부터
-            for(int j=0 ; j<i ; j++){ // 바로 이전 수의 증가 수열 탐색
-                if(arr[j][0] < arr[i][0]){ // 탐색하는 수가 현재 수보다 작으면서
-                    if(arr[j][1]+1 > arr[i][1]) { // 탐색 수가 자신의 최대증가수열에 포함된다면
-                        arr[i][1] = arr[j][1]+1; // 증가수열 길이를 탐색수+1로 정정해줌.
-                    }
-                }
-            }
+    int idx=n-1; // 주어진 동전 가치 최대값부터 역탐색하기
+    int cnt=0; // 필요한 동전갯수
+    while(k>0){
+        if(k>=arr[idx]){
+            int temp=k/arr[idx];
+            cnt+=temp;
+            k-=arr[idx]*temp;
         }
-        if(max<arr[i][1]) max=arr[i][1];
+        idx--;
     }
-    cout << max << endl;
+    cout << cnt << endl;
 }

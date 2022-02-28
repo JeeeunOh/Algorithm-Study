5e3fba246a157badf8ab1ad93f9692a8eeae7b5a
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-02-28 12:07:22 +0900

    물병 그리디

--- 개인공부/Greedy/boj1052_물병.cpp
+++ 개인공부/Greedy/boj1052_물병.cpp
@@ -2,34 +2,25 @@
 using namespace std; 
 int N, K; 
 
-// 전제조건: N은 주어진 물병의 개수, cnt: 들고 가는 물병, result: 상점에서 구매한 물병의 개수
-
-// 1. 주어진 물병을 최대한 합칩니다. 즉, N을 2로 나눕니다.
-// 2. 1번에서 나머지가 발생한다면 합쳐지지 않은 물병이 존재한다는 것이므로 cnt를 1 추가해줍니다.
-// 3. N이 0이 될 때까지 1번과 2번을 반복해줍니다.
-// 4. cnt의 개수가 K 이하일 경우 조건이 성립하므로 result를 출력해주고 프로그램을 종료합니다.
-// 5. 4번에서 조건이 성립하지 않을 경우 1 ~ 4번을 반복해줍니다.
-
-
 int main(void) { 
     ios_base::sync_with_stdio(0); 
     cin.tie(0); 
-    cin >> N >> K; 
+    cin >> N >> K; // N은 주어진 물병의 개수
     if (N <= K) { 
         cout << 0 << "\n"; 
         return 0; 
     } 
 
-    int result; 
+    int result; // result: 상점에서 구매한 물병의 개수
     
-    for (result = 0; ;result++) { 
-        int cnt = 0; 
+    for (result = 0; ; result++) { // 5. 4번에서 조건이 성립하지 않을 경우 1 ~ 4번을 반복해줍니다.
+        int cnt = 0; // cnt: 들고 가는 물병
         int tempN = N; 
-        while (tempN) { 
-            if (tempN % 2) cnt++;
-            tempN /= 2; 
+        while (tempN) { // 3. N이 0이 될 때까지 1번과 2번을 반복해줍니다.
+            if (tempN % 2) cnt++; // 2. 1번에서 나머지가 발생한다면 합쳐지지 않은 물병이 존재한다는 것이므로 cnt를 1 추가해줍니다.
+            tempN /= 2; // 1. 주어진 물병을 최대한 합칩니다. 즉, N을 2로 나눕니다.
         } 
-        if (cnt <= K) break;  
+        if (cnt <= K) break;  // 4. cnt의 개수가 K 이하일 경우 조건이 성립하므로 result를 출력해주고 프로그램을 종료합니다.
         N++; 
     } 
     

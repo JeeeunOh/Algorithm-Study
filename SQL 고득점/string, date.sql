c07ce5c79c3026e100aae12ccc5351bcf9f27704
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-03-02 15:16:56 +0900

    sql string, date

---
+++ SQL 고득점/string, date.sql
@@ -0,0 +1,49 @@
+-- [루시와 엘라 찾기]
+-- 동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 
+-- 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 SQL 문을 작성해주세요.
+
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID ASC
+
+-- [이름에 el이 들어가는 동물 찾기]
+-- 보호소에 돌아가신 할머니가 기르던 개를 찾는 사람이 찾아왔습니다. 
+-- 이 사람이 말하길 할머니가 기르던 개는 이름에 'el'이 들어간다고 합니다.
+-- 동물 보호소에 들어온 동물 이름 중, 이름에 EL이 들어가는 개의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 
+-- 이때 결과는 이름 순으로 조회해주세요. 단, 이름의 대소문자는 구분하지 않습니다.
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NAME LIKE '%el%' AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME
+
+-- [중성화 여부 파악하기]
+-- 보호소의 동물이 중성화되었는지 아닌지 파악하려 합니다. 
+-- 중성화된 동물은 SEX_UPON_INTAKE 컬럼에 'Neutered' 또는 'Spayed'라는 단어가 들어있습니다. 
+-- 동물의 아이디와 이름, 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성해주세요.
+-- 이때 중성화가 되어있다면 'O', 아니라면 'X'라고 표시해주세요.
+
+SELECT ANIMAL_ID, NAME, 
+        IF(SEX_UPON_INTAKE LIKE 'Neutered%' or SEX_UPON_INTAKE Like 'Spayed%', 'O', 'X') AS '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+-- [오랜 기간 보호한 동물(2)]
+-- 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 
+-- 이때 결과는 보호 기간이 긴 순으로 조회해야 합니다.
+
+SELECT AI.ANIMAL_ID, AI.NAME
+FROM ANIMAL_INS AS AI
+JOIN ANIMAL_OUTS AS AO
+ON AI.ANIMAL_ID = AO.ANIMAL_ID
+ORDER BY AO.DATETIME - AI.DATETIME DESC
+LIMIT 2
+
+-- [DATETIME에서 DATE로 형 변환]
+-- ANIMAL_INS 테이블에 등록된 모든 레코드에 대해, 각 동물의 아이디와 이름, 들어온 날짜1를 조회하는 SQL문을 작성해주세요. 
+-- 이때 결과는 아이디 순으로 조회해야 합니다.
+
+SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, '%Y-%m-%d')
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
---
+++ 개인공부/DFS_BFS/boj1600_말이되고픈원숭이.cpp
@@ -0,0 +1,84 @@
+// https://yabmoons.tistory.com/48
+#include<iostream>
+#include<queue>
+#define endl "\n"
+#define MAX 200
+using namespace std;
+ 
+int W, H, K;
+int MAP[MAX][MAX];
+bool Visit[MAX][MAX][31];
+// (x,y) 지점에 능력을 a번 사용해서 왔다.
+ 
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+int hdx[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+int hdy[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
+ 
+void Solve()
+{
+    cin >> K;
+    cin >> W >> H;
+    for (int i = 0; i < H; i++)
+        for (int j = 0; j < W; j++)
+            cin >> MAP[i][j];
+
+    // [ (x,y), (이동횟수, 능력사용횟수) ] 
+    queue<pair<pair<int, int>, pair<int, int>>> Q;
+    Q.push(make_pair(make_pair(0, 0), make_pair(0, 0)));
+    Visit[0][0][0] = true;
+ 
+    while (Q.empty() == 0){
+        int x = Q.front().first.first;
+        int y = Q.front().first.second;
+        int Cnt = Q.front().second.first;
+        int Ability = Q.front().second.second;
+        Q.pop();
+
+        // 끝점 도달하면 출력
+        if (x == H - 1 && y == W - 1){
+            cout << Cnt << endl;
+            return;
+        }
+        
+        // 말처럼 이동할 수 있는 횟수가 남아있다면
+        if (Ability < K) {
+            for (int i = 0; i < 8; i++) {
+                int nx = x + hdx[i];
+                int ny = y + hdy[i];
+                if (nx >= 0 && ny >= 0 && nx < H && ny < W) { // 탐색 좌표가 지정 범위 벗어나지 않았으면서
+                    if (MAP[nx][ny] == 0 && Visit[nx][ny][Ability + 1] == false) { // 해당 자리에 장애물 없고 방문한 적이 없으면
+                        Visit[nx][ny][Ability + 1] = true; // 방문한 적 있게 바꿔주고
+                        Q.push(make_pair(make_pair(nx, ny), make_pair(Cnt + 1, Ability + 1))); // 탐색하라고 큐에 넣어줌.
+                    }
+                }
+            }
+        }
+        // 원숭이처럼 이동 그 이후는 위와 같음
+        for (int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+ 
+            if (nx >= 0 && ny >= 0 && nx < H && ny < W) {
+                if (MAP[nx][ny] == 0 && Visit[nx][ny][Ability] == false) {
+                    Visit[nx][ny][Ability] = true;
+                    Q.push(make_pair(make_pair(nx, ny), make_pair(Cnt + 1, Ability)));
+                }
+            }
+        }
+    }
+
+    // while문에서 return 되지 않았으면 여기서 -1 출력
+    cout << -1 << endl;
+}
+ 
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    Solve();
+ 
+    return 0;
+}

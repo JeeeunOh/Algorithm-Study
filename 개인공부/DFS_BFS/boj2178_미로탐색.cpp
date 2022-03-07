8c3e7e1149d60b684ca7b020d52de097238f16b8
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-03-07 14:39:47 +0900

    bfs 개어렵다

--- 개인공부/DFS_BFS/boj2178_미로탐색.cpp
+++ 개인공부/DFS_BFS/boj2178_미로탐색.cpp
@@ -1,50 +1,66 @@
-#include <iostream>
+#include <stdio.h>
 #include <queue>
 using namespace std;
 
-queue<pair<pair<int, int>, <int>>> q;
-int dx[] = {1,-1,0,0};
-int dy[]= {0,0,1,-1};
+/*
+4 6
+101111
+101010
+101011
+111011
+*/
 
-int main(){
-    int n,m;
-    int arr[100][100];
-    cin >> n >> m;
+int N, M;
+int check[101][101]; // 시작점으로부터의 거리 표시
+int visit[101][101]; // 방문표시
+char map[101][101]; // 미로
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
 
-    for (int i=0 ; i<n ; i++){
-        int temp;
-        cin >> temp;
-        for(int j=m-1 ; j>=0 ; j--){
-            arr[i][j]=temp%10;
-            temp/=10;
-        }
-    }
+void BFS(int x, int y) 
+{
+	visit[x][y] = 1; // 방문표시
 
-    q.push(make_pair(make_pair(0,0), 0));
-    int min=100000;
-    bool check[100][100];
+	queue<pair<int, int> > q; // 큐 생성
+	q.push(make_pair(x, y)); // 시작점 넣기
 
-    while(!q.empty()){
-        int x = q.front().first.first;
-        int y = q.front().first.second;
-        int cnt=q.front().second;
+	while (!q.empty()) // 큐에 데이터가 없을때까지
+	{
+		int x = q.front().first;
+		int y = q.front().second;
 
-        check[x][y]=true;
-        q.pop();
-        
-        if(x==n-1 && y==m-1){
-            cnt++;
-            if(min>cnt) min=cnt;
-        }
+		q.pop(); // 큐 맨 앞 데이터 제거
 
-        for(int i=0 ; i<4;  i++){
-            int tx=x+dx[i];
-            int ty=y+dy[i];
+		for (int i = 0; i < 4; i++) 
+		{
+			int next_x = x + dx[i];
+			int next_y = y + dy[i];
 
-            if(arr[tx][ty]==1 && check[tx][ty]!=true){
-                q.push(make_pair(make_pair(tx,ty), cnt+1));
-            }
-        }
-    }
-    cout << min << endl;
+			if (0 <= next_x && next_x < N && 0 <= next_y && next_y < M) // 미로의 범위
+			{
+				// 길이 존재하고 이전에 방문했던적이 없는 경우
+				if (map[next_x][next_y] == '1' && visit[next_x][next_y] == 0)
+				{
+					check[next_x][next_y] = check[x][y] + 1;
+					visit[next_x][next_y] = 1;
+					q.push(make_pair(next_x, next_y)); // 큐에 데이터 밀어넣기
+				}
+
+			}
+
+		}
+	}
+
+}
+
+int main(void) 
+{
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%s", map[i]);
+	}
+
+	BFS(0, 0);
+	printf("%d\n", check[N - 1][M - 1] + 1);
 }

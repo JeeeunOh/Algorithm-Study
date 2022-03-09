3861ffd962c33d2d172a15eafda1b357f8d77689
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-03-09 22:22:18 +0900

    노가다문제...에서 도망가자

---
+++ 개인공부/DFS_BFS/boj2251_물통.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    
+    int arr[3]={0};
+    int max[3];
+
+    for(int i=0 ; i<3 ; i++){
+        cin >> max[i];
+        if(i==2) arr[i]=max[i];
+    }
+    
+
+}

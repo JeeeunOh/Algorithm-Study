6df6e0be28f4456b64f4290e19df6acfe0ae4a00
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-03-09 15:13:05 +0900

    bfs 죽이자

---
+++ 개인공부/DFS_BFS/boj7562_나이트의이동.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int dx[] = {1,2,2,1,-1,-2,-2,-1}; 
+int dy[] = {2,1,-1,-2,-2,-1,1,2};
+
+int main(){
+    int T;
+    cin >> T;
+
+    while(T--){
+        int n;
+        int x1, y1, x2, y2;
+        int check[301][301];
+        queue<pair<int,int>> q;
+
+        
+        cin >> n;
+        cin >> x1 >> y1 >> x2 >> y2;
+        check[x1][y1]=1;
+
+        q.push(make_pair(x1, y1));
+
+        while(!q.empty()){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+
+            for(int i=0 ; i<8 ; i++){
+                int tx=x+dx[i];
+                int ty=y+dy[i];
+
+                if(tx>=0 && tx<n && ty>=0 && ty<n && !check[tx][ty]){
+                    q.push({tx, ty});
+                    check[tx][ty]=check[x][y]+1;
+                }
+            }
+        }
+        cout << check[x2][y2]-1 << endl;
+    }
+}

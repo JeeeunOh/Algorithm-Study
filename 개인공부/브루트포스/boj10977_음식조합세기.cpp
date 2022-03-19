00fae9c9487b78874ea0dee3741430e91e5ced0d
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-03-19 12:50:57 +0900

    다익스트라 개념 공부

---
+++ 개인공부/브루트포스/boj10977_음식조합세기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    int m,n;
+    cin >> t;
+
+    while(t--){
+        cin >> m >> n; // 최대가짓수, 끼니마다 제공되는 음식 수
+        long long food[1000001];
+        for(int i=0 ; i<n ; i++){
+            cin >> food[i]; // 끼니에 제공된 음식 번호
+        }
+
+
+    }
+}

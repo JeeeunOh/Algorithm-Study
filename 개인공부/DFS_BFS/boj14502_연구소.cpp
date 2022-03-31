061f47f102b9bf09d0870826c4690e8409e31bab
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-03-31 12:35:24 +0900

    연구소 내 바이러스

--- 개인공부/DFS_BFS/boj14502_연구소.cpp
+++ 개인공부/DFS_BFS/boj14502_연구소.cpp
@@ -1,23 +1,95 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
 using namespace std;
 
-int dx[]={-1,1,0,0};
-int dy[]={0,0,-1,1};
+const int MAX = 8;
 
-queue<pair<pair<int, int>, int>> q; // x좌표, y좌표, 남은 벽 갯수
+typedef struct{
+    int y, x;
+}Dir;
 
-// 0은 빈칸, 1은 벽, 2는 바이러스
+Dir moveDir[4] = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
 
-int main(){
-    int N,M;
-    cin >> N >> M;
-    int arr[8][8];
 
-    for(int i=0 ; i<N; i++){
-        for(int j=0 ; j<M; j++){
-            cin >> arr[i][j];
+int N, M;
+int lab[MAX][MAX];
+int temp[MAX][MAX];
+int result;
+
+void BFS(void){
+    int afterSpread[MAX][MAX];
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            afterSpread[i][j] = temp[i][j];
+            
+    queue<pair<int, int>> q; //y, x
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            if (afterSpread[i][j] == 2) //바이러스라면
+                q.push(make_pair(i, j)); //큐에 넣는다 
+                
+    while (!q.empty()){
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
+        
+        for (int i = 0; i < 4; i++){
+            int nextY = y + moveDir[i].y;
+            int nextX = x + moveDir[i].x;
+            
+            if(0 <= nextY && nextY < N && 0 <= nextX && nextX < M) //범위 내
+                if (afterSpread[nextY][nextX] == 0){
+                    afterSpread[nextY][nextX] = 2; //바이러스 감염
+                    q.push(make_pair(nextY, nextX));
+                }
+            }
         }
+
+        int empty = 0;
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+                if (afterSpread[i][j] == 0)
+                    empty++;
+                    
+        result = max(result, empty);
+}
+
+ 
+
+void makeWall(int cnt){
+    if (cnt == 3) {//벽을 세개 만들었으므로
+        BFS();
+        return;
     }
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++)
+            if (temp[i][j] == 0){
+                temp[i][j] = 1; //마찬가지로 해당칸에 새우고
+                makeWall(cnt + 1);
+                temp[i][j] = 0; //다시 허문다
+            }
+}
+
+int main(void){
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            cin >> lab[i][j];
+
+    for(int i=0; i<N; i++)
+        for(int j=0; j<M; j++)
+            if (lab[i][j] == 0) { //빈칸 발견 시 연구실 상태를 복사
+                for (int k = 0; k < N; k++)
+                    for (int l = 0; l < M; l++)
+                        temp[k][l] = lab[k][l];
+                temp[i][j] = 1; //해당 칸에 벽을 세운다
+                makeWall(1); //벽을 세운 상태이므로 cnt = 1
+                temp[i][j] = 0; //다시 허문다
+            }
+
+    cout << result << endl;
+    return 0;
 
 }

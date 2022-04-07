c3d75ab366f9f4f78fe1f0d53ee746fdc81372f2
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-04-07 11:05:08 +0900

    삼차원 배열 bfs

--- 개인공부/DFS_BFS/boj7569_토마토.cpp
+++ 개인공부/DFS_BFS/boj7569_토마토.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
-int main(){
-    
+typedef struct {
+        int x, y, z;
+        int cnt;
+} types;
+queue<types> bfs;
+
+int M, N, H; //가로 세로 높이
+int box[101][101][101];
+bool visited[101][101][101];
+int dx[6] = {1, -1, 0, 0, 0, 0};
+int dy[6] = {0, 0, 1, -1, 0, 0};
+int dz[6] = {0, 0, 0, 0, 1, -1};
+
+
+int main(void) {
+        int cnt = 0;
+        cin >> M >> N >> H;
+        for (int k = 0; k < H; k++)
+                for (int i = 0; i < N; i++)
+                        for (int j = 0; j < M; j++) {
+                            cin >> box[i][j][k];
+                            if (box[i][j][k] == 1) {
+                                bfs.push({i, j, k, 0});
+                                visited[i][j][k] = true;
+                            }
+                            else if (box[i][j][k] == -1)
+                                visited[i][j][k] = true;
+                        }
+        if (bfs.empty()) {
+                printf("-1");
+                return 0;
+        }
+
+        while (!bfs.empty()) {
+                auto a = bfs.front();
+                bfs.pop();
+                cnt = max(cnt, a.cnt);
+
+                for (int i = 0; i < 6; i++) {
+                    int nx = a.x + dx[i];
+                    int ny = a.y + dy[i];
+                    int nz = a.z + dz[i];
+
+                    if (nx >= 0 && ny >= 0 && nz >= 0 && nx < N && ny < M && nz < H && !visited[nx][ny][nz]) {
+                        bfs.push({nx, ny, nz, a.cnt + 1});
+                        visited[nx][ny][nz] = true;
+                    }
+                }
+        }
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+                for(int k = 0; k < H; k++) 
+                    if (!visited[i][j][k]) {
+                        printf("-1");
+                        return 0;
+                    }
+        cout << cnt << '\n';
+        return 0;
+                        
 }

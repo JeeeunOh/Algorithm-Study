759e682d3508e86c0a6add9366a7a5b7ccf3c798
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-04-12 12:19:38 +0900

    가위바위보 커밋.. 집가서 마저 하기

---
+++ 개인공부/Greedy/boj2930_가위바위보.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int n,r;
+    char arr[3]={'S', 'P', 'R'}; // 가위 0, 보 1, 바위 2
+    string sang, frd; // 상근이, 친구
+    cin >> r >> sang >> n ; // 라운드 수, 상근이 조합, 친구 수
+
+    int final=0;
+    int MAX=0;
+
+    while(n--){
+        int temp=0;
+        string frd;
+        cin >> frd;
+        
+        for(int i=0 ; i<sang.length(); i++){
+            int sangN, frdN;
+            for(int j=0 ; j<3 ; j++){
+                if(arr[j]==sang[i]) sangN=j;
+                if(arr[j]==frd[i]) frd=j;
+            }
+            if(sangN-frdN==-1 || sangN-frdN==2) temp+=2;
+            else if(sangN==frdN) temp+=1;
+        }
+        final+=temp;
+        if(MAX<temp) MAX=temp; 
+    }
+}

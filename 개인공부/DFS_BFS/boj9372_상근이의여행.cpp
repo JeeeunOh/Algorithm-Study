9a996e2222eb51a5f28662ec02e1766b40d94d4b
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-05-04 20:10:22 +0900

    막상 쉬웠던 문제

---
+++ 개인공부/DFS_BFS/boj9372_상근이의여행.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+
+    while(T--){
+        int N,M;
+        int cnt=0;
+        int res=0;
+        cin >> N >> M;
+
+        while(M--){
+            int a,b;
+            cin >> a >> b;
+        }
+
+        cout << N-1 << '\n';
+    }
+}

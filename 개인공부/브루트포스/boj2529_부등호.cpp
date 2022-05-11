c0d641dd49080bc2be2514c964c1dfa5f19a9691
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-05-11 15:35:33 +0900

    순열 이용한 브루트포스

--- 개인공부/브루트포스/boj2529_부등호.cpp
+++ 개인공부/브루트포스/boj2529_부등호.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-int main(){
+// 부등호 검사
+bool check(vector<int> &perm, vector<char> &vec){
+    for(int i=0; i<vec.size(); i++){
+        if(vec[i]=='<' && perm[i] > perm[i+1]) return false;
+        if(vec[i]=='>' && perm[i] < perm[i+1]) return false;
+    }
+    return true;
+}
+
+int main(void){
+    int k;
+    cin >> k;
     
+    vector<char> vec(k); // 부등호 저장
+    for(int i=0; i<k; i++){
+        cin >> vec[i];
+    }
+    
+    vector<int> small(k+1); // 가장 작은 수 k+1 개 저장 : 0,1,2, ..
+    vector<int> big(k+1); // 가장 큰 수 k+1 개 저장 : 9,8,7, ..
+    
+    for(int i=0; i<=k; i++){
+        small[i] = i;
+        big[i] = 9-i;
+    }
+    
+    // 작은 수 순열 구하기
+    do{
+        if(check(small, vec)) break;
+    }while(next_permutation(small.begin(),small.end()));
+    
+    // 큰 수 순열 구하기
+    do{
+        if(check(big, vec)) break;
+    }while(prev_permutation(big.begin(), big.end()));
+    
+    // 최대 정수 출력
+    for(int i=0; i<big.size(); i++){
+        cout << big[i];
+    }
+    cout << '\n';
+    
+    // 최소 정수 출력
+    for(int i=0; i<small.size(); i++){
+        cout << small[i];
+    }
 }

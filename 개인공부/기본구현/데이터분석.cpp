2aa73e887ff1f1fa5622451dd7b247c8d0941b3c
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-05-16 13:19:44 +0900

    데이터분석 끝

--- 개인공부/기본구현/데이터분석.cpp
+++ 개인공부/기본구현/데이터분석.cpp
@@ -5,12 +5,17 @@ using namespace std;
 int main(){
     int n=10;
     int arr[20];
+    int arr1[20];
 
     for(int i=0 ; i<n ; i++){
         cin >> arr[i];
     }
+     for(int i=0 ; i<n ; i++){
+        cin >> arr1[i];
+    }
+    for(int i=0 ; i<n ; i++){
+        cout << arr[i]-arr1[i] << " ";
+    }
 
-    sort(arr, arr+n);
-
-    cout << arr[n/2] << '\n';
+    cout <<'\n';
 }

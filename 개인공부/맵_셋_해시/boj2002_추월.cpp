e0f7eb056375bd28634ba8bb87a91599b393a5d3
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-08-14 00:41:43 +0900

    반례를 생각해보자

--- 개인공부/맵_셋_해시/boj2002_추월.cpp
+++ 개인공부/맵_셋_해시/boj2002_추월.cpp
@@ -7,26 +7,34 @@ int main(){
     cin >> n;
 
     map<string, int> front;
-    map<string, int> rear;
+    map<int, string> rear;
 
     for(int i=0 ; i<n ; i++){
         string s;
         cin >> s;
-        front.insert({s,i});
+        front.insert({s, i});
     }
     
     for(int i=0 ; i<n ; i++){
         string s;
         cin >> s;
-        rear.insert({s,i});
+        rear.insert({i, s});
     }
 
     int res=0;
 
-    for(auto iter : front){
-         int f = front[iter.first];
-         int r = rear[iter.first];
-         if(f-r>0) res++;
+    for(auto iter : rear){ // 터널 후에 나온 차들 탐색
+        int cur = iter.first;
+        for(int i=cur+1 ; i<n ; i++){ // 해당 차의 뒤에 나온 차들 다 탐색
+            string next = rear[i];
+            int frontNext = front[next]; // 뒤에나온차가 터널 전에는 몇 등이었는지
+            int frontCur = front[iter.second]; // 자신은 터널 전에 몇 등이었는지
+            if(frontNext < frontCur) { // 원래 (자신 앞에 있던 차였으면 == 추월했으면) ++ 해주기
+                res++;
+                break;
+            }
+        }
     }
+
     cout << res << '\n';
 }

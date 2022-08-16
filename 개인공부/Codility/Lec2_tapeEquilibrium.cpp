ca3e0ae5ceaf6cd2b41bd07662888d343ba80582
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-08-17 08:14:41 +0900

    역for문 시 idx에 주의하자
    돌아돌아 개고생,,,

---
+++ 개인공부/Codility/Lec2_tapeEquilibrium.cpp
@@ -0,0 +1,34 @@
+int solution(vector<int> &a) {
+    int headsum[a.size()+1];
+    int tailsum[a.size()+1];
+
+    headsum[0]=0;
+    tailsum[a.size()]=0;
+
+    for(int i=1 ; i<=a.size() ; i++){
+        headsum[i]=headsum[i-1]+a[i-1];
+        // cout << headsum[i] << " ";
+    }
+    // cout << '\n';
+
+    for(int i=a.size()-1; i>=0 ; i--){
+        tailsum[i]=tailsum[i+1]+a[i];
+    }
+
+    // for(int i=1 ; i<=a.size() ; i++){
+    //     cout << tailsum[i] << " ";
+    // }
+    // cout << '\n';
+
+    int min=100000000;
+    for(int i=1; i<a.size(); i++){
+        int temp = headsum[i]-tailsum[i];
+        if(temp<0) temp*=(-1);
+        if(min>temp) {
+            // cout << i << " " << headsum[i] <<  " " << tailsum[i] << '\n';
+            min = temp;
+        }
+    }
+    
+    return min;
+}

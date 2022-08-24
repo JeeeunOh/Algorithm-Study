818b754f02e97be22dc651dc387600f33884d3c3
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-08-24 19:58:27 +0900

    생각해야 하는 dp

---
+++ 개인공부/브루트포스/boj12865_평범한배낭.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+    vector<int> dp(k+1, 0); // 무게가 w일 때의 가치
+
+    for(int i=0 ; i<n ; i++){
+        int a,b;
+        cin >> a >> b; // 무게 가치
+        for(int j=k ; j>=a ; j--){
+            dp[j]=max(dp[j], dp[j-a]+b);
+        }
+    }
+    cout << dp[k] << '\n';
+}

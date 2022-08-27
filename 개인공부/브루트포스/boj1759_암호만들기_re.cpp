878d4a61d3bbbecbb957599c7996014f3c8d49f9
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-08-27 15:49:05 +0900

    dfs로 암호 풀기

--- 개인공부/브루트포스/boj1759_암호만들기_re.cpp
+++ 개인공부/브루트포스/boj1759_암호만들기_re.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <iostream>
 #include <algorithm>
 using namespace std;
 
@@ -6,19 +6,19 @@ int l,c;
 int res[50];
 char arr[50];
 
-void dfs(int x,int y){
+void dfs(int idx,int len){
 
-    if(y==l){
+    if(len==l){
         int mo=0;
         int ja=0;
 
-        for(int i=0;i<y;i++){
+        for(int i=0;i<len;i++){
             if(arr[res[i]]=='a'||arr[res[i]]=='e'||arr[res[i]]=='i'||arr[res[i]]=='o'||arr[res[i]]=='u') mo++;
             else ja++;
         }
 
         if(mo>=1&&ja>=2){
-            for(int i=0;i<y;i++){
+            for(int i=0;i<len;i++){
                 printf("%c",arr[res[i]]);
             }
             printf("\n");
@@ -27,18 +27,18 @@ void dfs(int x,int y){
     }
 
     else{
-        for(int i=x;i<c;i++){
-            res[y]=i;
-            dfs(i+1,y+1);
+        for(int i=idx;i<c;i++){
+            res[len]=i;
+            dfs(i+1,len+1);
         }
     }
 }
 
 
 int main() {
-    scanf("%d %d",&l,&c);
+    cin >> l >> c;
 
-    for(int i=0;i<c;i++) scanf(" %c",&arr[i]);
+    for(int i=0;i<c;i++) cin >> arr[i];
 
     sort(arr,arr+c);
     dfs(0,0);

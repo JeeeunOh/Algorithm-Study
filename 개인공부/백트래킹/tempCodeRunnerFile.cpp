0a03b80b49418ce5df2ead9508d4df09f89ebb9e
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-08-27 16:23:17 +0900

    쓸데없이 헤맸지만 성공

---
+++ 개인공부/백트래킹/tempCodeRunnerFile.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n,m;
+vector<int> v(8);
+vector<int> output(8);
+
+void dfs(int idx, int cnt){
+    output[cnt]=v[idx];
+    cnt++;
+
+    if(cnt==m){
+        for(int i=0 ; i<m ; i++){
+            cout << output[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=idx+1 ; i<n ; i++){
+        dfs(i, cnt);
+    }
+
+}
+
+int main(){
+    cin >> n >> m;
+
+    for(int i=1 ; i<=n ; i++){
+        v[i-1]=i;
+        // cout << v[i-1] << " ";
+    }
+    cout << '\n';
+
+    for(int i=0 ; i<n ; i++){
+        dfs(i, 0);
+    }
+}

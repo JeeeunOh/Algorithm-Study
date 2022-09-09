aca5e7366ef2760bf851d86f656d0ed5f8002068
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-09-09 23:06:47 +0900

    문자열 다루기

--- 개인공부/문자열/boj1541_잃어버린괄호.cpp
+++ 개인공부/문자열/boj1541_잃어버린괄호.cpp
@@ -17,34 +17,26 @@ int main(){
     int res=0;
     int temp=0;
 
-    // cout << s << '\n';
-
     for(int i=0 ; i<s.length(); i++){
         if(s[i]=='-' || s[i]=='+') {
             if(last=='+') {
-                res+=temp;  
-                // cout << i << " " << res << " + " << temp<< '\n';
+                res+=temp;
             }
             else {
                 res-=temp;
-                // cout << i << " " << res << " - " << temp<< '\n';
             }
             last=s[i];
             temp=0;
-            // cout << res << '\n';
         } else {
             int t = temp*10 + (s[i]-'0');
             temp = t;
-            // cout << i << " : " << t << " " << temp << '\n';
         }
     }
 
     if(last=='+') {
         res+=temp;  
-        // cout << i << " " << res << " + " << temp<< '\n';
     } else {
         res-=temp;
-        // cout << i << " " << res << " - " << temp<< '\n';
     }
 
     cout << res << '\n';
---
+++ 개인공부/백트래킹/boj15649_N과M(1).cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+
+int main(){
+    cin >> n >> m;
+}

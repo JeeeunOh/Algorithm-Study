80e3f27cf7e90a99159c3583837391f1d5a76f0b
Jieun Oh <0909oje@naver.com>
2022-12-11 20:58:53 +0900

    map은 새 배열 생성에도 쓰임. 함수 내 조건에 따라 계산한 값이 배열에 들어감.

--- JavaScript/기능개발.js
+++ JavaScript/기능개발.js
@@ -36,3 +36,23 @@ function solution(pro, speeds) {
 
   return answer;
 }
+
+// 프로그래머스 다른 사람 풀이
+
+function solution(pro, speeds) {
+  let answer = [0];
+  // 각각 며칠 걸리는지 days에 저장.
+  let days = pro.map((item, index) => Math.ceil((100 - item) / speeds[index]));
+  let maxDay = days[0];
+
+  for (let i = 0, j = 0; i < days.length; i++) {
+    if (days[i] <= maxDay) {
+      answer[j] += 1;
+    } else {
+      maxDay = days[i];
+      answer[++j] = 1;
+    }
+  }
+
+  return answer;
+}

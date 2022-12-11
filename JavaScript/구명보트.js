4866c483b20639044f1906237385e6d15a3aded8
Jieun Oh <0909oje@naver.com>
2022-12-11 20:02:58 +0900

    4번 시도 끝에 성공... 조건을 잘 보자

---
+++ JavaScript/구명보트.js
@@ -0,0 +1,84 @@
+function solution(people, limit) {
+  // 사람 몸무게 수, 무게 제한
+  let cnt = 0;
+  people.sort((a,b)=>b-a); // 내림차순 정렬
+  
+  while(people.length!=0){
+      let temp = limit;
+      cnt++;
+      
+      people = people.filter((item)=>{
+          if(temp>0 && item<=temp){
+              temp-=item;
+              return false;
+          } else {
+              return true;
+          }
+      })
+  }
+  
+  return cnt;
+}
+
+// 정확도 100 , 효율성 시간초과 5/5
+
+function solution(people, limit) {
+  // 사람 몸무게 수, 무게 제한
+  let cnt = 0;
+  people.sort((a,b)=>b-a); // 내림차순 정렬
+    
+    for(let i=0 ; i<people.length; i++){
+        if(people[i]!=0){
+            cnt++;
+            for(let j=i+1 ; j<people.length; j++){
+                if(people[j]!=0 && people[i]+people[j]<=limit){
+                    people[i] = 0;
+                    people[j] = 0 ;
+                    break;
+                    
+                }
+            }
+            
+        }
+
+    }
+  
+  return cnt;
+}
+
+// 정확도 통과 100, 효용성 시간 초과 3/5
+function solution(people, limit) {
+  // 사람 몸무게 수, 무게 제한
+  let cnt = 0;
+  people.sort((a,b)=>b-a); // 내림차순 정렬
+    
+    while(people.length>0){
+        let len = people.length;
+        cnt++;
+        if(len==1) break;
+        if(people[0]+people[len-1]<=limit){
+            people.pop();
+            
+        } 
+        people.shift()
+    }
+  
+  return cnt;
+}
+
+// 굳이 앞에 것을 뺼 필요는 없다.
+// 그냥 앞에서부터 하나씩 탐색하면서 뒤에 것을 뺴자.
+// 모두 통과
+function solution(people, limit) {
+  // 사람 몸무게 수, 무게 제한
+  let cnt = 0;
+  people.sort((a,b)=>b-a); // 내림차순 정렬
+
+    for(let i=0 ; i<people.length; i++){
+        cnt++;
+        if(people[i]+people[people.length-1] <= limit)
+            people.pop();
+    }
+  
+  return cnt;
+}

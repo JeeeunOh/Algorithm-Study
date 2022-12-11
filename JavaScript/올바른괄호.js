53070f485772a73518e20e664cd99392e598016f
Jieun Oh <0909oje@naver.com>
2022-12-11 14:51:01 +0900

    시간초과 주의, 스택 문제

--- JavaScript/올바른괄호.js
+++ JavaScript/올바른괄호.js
@@ -17,4 +17,22 @@ function solution(s){
   return true;
 }
 
-// 
+// 시간 초과 해결
+// -> else if (item=='(') arr.push(item); 부분을 첫번쨰 테스트케이스에 묶어줬다..ㅎ
+
+function solution(s){
+  let arr = [];
+  
+  s.split('').map((item)=>{
+      let len = arr.length;
+      if(len==0 || item=='(') arr.push(item);
+      else if (item==')'){
+          if(arr[len-1] == '(') arr.pop();
+          else return false;
+      }
+  })
+  
+  if(arr.length!=0) return false;
+
+  return true;
+}

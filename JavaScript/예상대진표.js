261e85b5b044960c14250058d59823ca0f049613
오지은 <0909oje@naver.com>
2022-12-17 10:53:10 +0900

    floor가 아닌 ceil 이용한 함수

--- JavaScript/예상대진표.js
+++ JavaScript/예상대진표.js
@@ -6,3 +6,14 @@ function solution(n, a, b) {
     } else temp++;
   }
 }
+
+function solution(n, a, b) {
+  let answer = 0;
+  while (a !== b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    answer++;
+  }
+
+  return answer;
+}

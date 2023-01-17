8229df880ca1ca8baf9c308bfa26666e5d00bf97
JeeeunOh <0909oje@naver.com>
2023-01-17 21:15:24 +0900

    반례 케이스의 중요성

--- JavaScript/마법의엘리베이터.js
+++ JavaScript/마법의엘리베이터.js
@@ -1,18 +1,18 @@
 // 76.9
 
-function solution(storey) {
+function solution(st) {
   let cnt = 0;
 
-  while (storey) {
-    let last = storey % 10;
-    storey /= 10;
-    storey = Math.floor(storey);
+  while (st) {
+    let last = st % 10;
+    st /= 10;
+    st = Math.floor(st);
 
     if (last <= 5) {
       cnt += last;
     } else {
       cnt += 10 - last;
-      storey++;
+      st++;
     }
   }
 
@@ -24,22 +24,21 @@ function solution(storey) {
 // 75 -> 8
 // 555 -> 14
 
-// 끝이 5인 경우 일의 자리수만 생각하는게 아니라 십의 자
-function solution(storey) {
+// 100점
+function solution(st) {
   let cnt = 0;
 
-  while (storey) {
-    let last = storey % 10;
-    storey /= 10;
-    storey = Math.floor(storey);
+  while (st) {
+    let last = st % 10;
+    st /= 10;
+    st = Math.floor(st);
 
-    if (last < 5 || (last == 5 && (storey % 10) + 1 <= 5)) {
+    if (last < 5 || (last == 5 && (st % 10) + 1 <= 5)) {
       cnt += last;
-    } else if (last > 5 || (last == 5 && (storey % 10) + 1 > 5)) {
+    } else if (last > 5 || (last == 5 && (st % 10) + 1 > 5)) {
       cnt += 10 - last;
-      storey++;
+      st++;
     }
   }
-
   return cnt;
 }

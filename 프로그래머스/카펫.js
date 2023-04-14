bf645154d6c43d942c48aa2af27d380932d67ca2
Jieun Oh <0909oje@naver.com>
2023-04-14 20:23:23 +0900

    feat: 단순구현

--- 프로그래머스/카펫.js
+++ 프로그래머스/카펫.js
@@ -1,3 +1,17 @@
+// 전부 통과함.
+function solution(brown, yellow) {
+  var answer = [];
+  let width = 3, height = 3; // yellow 1 이상, brown 8이상이려면 적어도 3x3이어야함.
+  
+  while(true){
+      height = (brown+4)/2 - width;
+      if((width-2)*(height-2)===yellow && width>=height) break;
+      else width++;
+  }
+  
+  return [width, height];
+}
+
 // 2개만 통과함.
 function solution(brown, yellow) {
   var answer = [];

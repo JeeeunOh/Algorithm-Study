4c33d7188952fd83283515332dd0be3cfcce5b7f
Jieun Oh <0909oje@naver.com>
2023-04-14 16:00:18 +0900

    feat: 투포인터 풀이

--- 프로그래머스/숫자의표현.js
+++ 프로그래머스/숫자의표현.js
@@ -1,3 +1,24 @@
+// 투포인터 풀이
+function solution(n) {
+  let answer = 0;
+  let start = 1, end=1, sum=1; // start~end까지 더한 값
+  
+  while(start<=end && end!=n+1 && start>=1){ // start가 end보다 작거나 같아야 함. start, end가 정해진 지점 넘어가면 안됨.
+      if(sum<n){ // sum이 작으면, end 바로 뒤의 숫자를 더함
+          end++; 
+          sum+=end;
+      } else if (sum>n){ // sum이 크면, start를 뺌.
+          sum-=start;
+          start++;
+      } else if (sum===n){ // sum===n이면 answer 추가하고 end를 뒤로 미룸.
+          answer++;
+          end++;
+          sum+=end;
+      }
+  }
+  return answer;
+}
+
 function solution(n) {
   let cnt = 0;
   let dp = [0];

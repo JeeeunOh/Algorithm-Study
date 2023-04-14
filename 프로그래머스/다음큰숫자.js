8dd073ded23830f4dd4b7f34b6d31a19213d894c
Jieun Oh <0909oje@naver.com>
2023-04-14 16:00:04 +0900

    feat: 단순구현문제

--- 프로그래머스/다음큰숫자.js
+++ 프로그래머스/다음큰숫자.js
@@ -1,3 +1,18 @@
+// 14:40 ~ 
+// 1. 첫 풀이 : 14:44 완성
+// 2. 시간초과 생각 ~ : 1000000 넣었을 때 결과가 1초 안에 나옴 -> 패스
+// 3. 최종 풀이 : 14:46
+function solution(n) {
+  let temp = n+1;
+  let nLen = String(n.toString(2)).split('').filter((item)=>item!='0').length;
+
+  while(true){
+      let tempLen = String(temp.toString(2)).split('').filter((item)=>item!='0').length;
+      if(tempLen===nLen) return temp;
+      temp++;
+  }
+}
+
 function solution(n) {
   function countOne(input) {
     let sum = 0;

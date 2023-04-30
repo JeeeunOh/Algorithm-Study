c5d79d5872c207383436b4e54710b5d833fdf550
Jieun Oh <0909oje@naver.com>
2023-04-30 22:37:22 +0900

    feat: JS-Algorithm-Study에서 풀었던 것들 업로드

---
+++ BAEKJOON/JAVASCRIPT/boj1965_상자넣기.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = fs.readFileSync('/Users/jieun/Documents/GitHub/JS_ALGORITHM_STUDY/JeeeunOh/230428/input.txt').toString().trim().split('\n');

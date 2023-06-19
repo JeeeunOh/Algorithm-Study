7930088249152de846e685a357948cdff3dad689
Jieun Oh <0909oje@naver.com>
2023-06-19 11:43:14 +0900

    refactor: 프로그래머스 파일명 정리

--- 프로그래머스/pro_최댓값과최솟값.py
+++
@@ -1,6 +0,0 @@
-def solution(s):
-    answer = ''
-    arr = list(map(int, s.split()))
-    arr.sort()
-    
-    return str(arr[0]) + " " + str(arr[-1])
--- 프로그래머스/타겟넘버.js
+++ 프로그래머스/타겟넘버.js
@@ -19,3 +19,20 @@ function solution(numbers, target) {
 
   return answer;
 }
+
+function solution(numbers, target) {
+  let answer = 0;
+
+  const dfs = (curIdx, sum) => {
+    if (curIdx === numbers.length) {
+      if (sum === target) answer++;
+      return;
+    }
+    dfs(curIdx + 1, sum - numbers[curIdx]);
+    dfs(curIdx + 1, sum + numbers[curIdx]);
+  };
+
+  dfs(0, 0);
+
+  return answer;
+}

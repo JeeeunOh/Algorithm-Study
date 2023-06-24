20cd74132a8d89ce10141f2cc3e1d255e3c45af6
Jieun Oh <0909oje@naver.com>
2023-06-25 00:25:04 +0900

    feat: 구현 문제

--- 프로그래머스/다음큰숫자.js
+++ 프로그래머스/다음큰숫자.js
@@ -1,15 +1,35 @@
-// 14:40 ~ 
+function solution(n) {
+  let n_len = n
+    .toString(2)
+    .split('')
+    .filter((item) => item === '1').length;
+  let ans = n + 1;
+  while (true) {
+    let newN_len = ans
+      .toString(2)
+      .split('')
+      .filter((item) => item === '1').length;
+    if (newN_len === n_len) return ans;
+    ans++;
+  }
+}
+
+// 14:40 ~
 // 1. 첫 풀이 : 14:44 완성
 // 2. 시간초과 생각 ~ : 1000000 넣었을 때 결과가 1초 안에 나옴 -> 패스
 // 3. 최종 풀이 : 14:46
 function solution(n) {
-  let temp = n+1;
-  let nLen = String(n.toString(2)).split('').filter((item)=>item!='0').length;
+  let temp = n + 1;
+  let nLen = String(n.toString(2))
+    .split('')
+    .filter((item) => item != '0').length;
 
-  while(true){
-      let tempLen = String(temp.toString(2)).split('').filter((item)=>item!='0').length;
-      if(tempLen===nLen) return temp;
-      temp++;
+  while (true) {
+    let tempLen = String(temp.toString(2))
+      .split('')
+      .filter((item) => item != '0').length;
+    if (tempLen === nLen) return temp;
+    temp++;
   }
 }
 
